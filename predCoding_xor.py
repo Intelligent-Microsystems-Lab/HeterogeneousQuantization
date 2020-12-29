83df2649d87108df6c0e55e4424c7d975c90a162
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-12-29 17:35:55 +0100

    structure

--- predCoding_xor.py
+++ predCoding_xor.py
@@ -6,6 +6,9 @@ import jax
 from torchvision import datasets, transforms
 import torch
 
+# module load python cuda/10.2
+# export XLA_FLAGS="--xla_gpu_cuda_data_dir=/afs/crc.nd.edu/x86_64_linux/c/cuda/10.2"
+
 @jit
 def rmse(sout, pred):
     return jnp.sqrt(jnp.mean((sout - pred)**2))
