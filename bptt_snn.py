457cddc63cff243c37a0ce80fb612f90431ecf90
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2021-01-04 16:00:22 -0500

    test

--- bptt_snn.py
+++ bptt_snn.py
@@ -1,4 +1,4 @@
-import argparse, time, pickle, uuid
+import argparse, time, pickle, uuid, os
 
 from functools import partial
 import jax.numpy as jnp
@@ -58,7 +58,44 @@ def vr_loss(alpha_vr, pred, target):
 
     return jnp.reshape(jnp.sqrt(1/5e-3*jnp.sum((c_pred - c_target)**2)), ())
 
-def pattern_plot(sin, sout, pred):
+def cla_loss(alpha_vr, pred, target):
+    return jnp.mean(jnp.sum(pred * target, axis=1))
+
+def run_snn(weights, biases, alpha, gamma, thr, x_train):
+    T = x_train.shape[0]
+    mem = [jnp.zeros(l.shape[0]) for l in weights]
+    out_s = jnp.empty((0, weights[-1].shape[0]))
+
+    for t in range(T):
+        st = x_train[t,:]
+        for i, (w, b) in enumerate(zip(weights, biases)):
+            mem[i] = alpha * mem[i] + jnp.dot(weights[i], st) + biases[i]
+            st = spike_nonlinearity(mem[i], thr)
+            mem[i] -= st * gamma
+        out_s = jnp.vstack((out_s, st))
+    return out_s
+
+v_run_snn = jit(vmap(run_snn, (None, None, None, None, None, 0)), static_argnums=[2, 3, 4])
+
+def loss_pred(weights, biases, alpha, gamma, alpha_vr, thr, x_train, y_train, loss_fn):
+    pred_s = v_run_snn(weights, biases, alpha, gamma, thr, x_train)
+    return loss_fn(alpha_vr, pred_s, y_train).sum(0)
+
+@jax.partial(jit, static_argnums=[2, 3, 4, 5, 8])
+def update_w(weights, biases, alpha, gamma, alpha_vr, thr, x_train, y_train, loss_fn):
+    loss, gwb = value_and_grad(loss_pred, argnums= (0,1))(weights, biases, alpha, gamma, alpha_vr, thr, x_train, y_train, loss_fn)
+    weights = [w - args.l_rate * gw for w, gw in zip(weights, gwb[0])]
+    biases = [b - args.l_rate * gb for b, gb in zip(biases, gwb[1])]
+
+    return loss, weights, biases
+
+@jit
+def acc_compute(pred, target):
+    return jnp.mean(pred.sum(1).argmax(1) == target.sum(1).argmax(1))
+
+    #return jnp.mean(pred == target)
+
+def pattern_plot(sin, sout, pred, name, textl):
     fig, axes = plt.subplots(nrows=1, ncols=4)
     axes[0].imshow(sin.astype(float))
     axes[0].set_title("Input")
@@ -69,24 +106,40 @@ def pattern_plot(sin, sout, pred):
     axes[3].imshow(pred.astype(float) - sout.astype(float))
     axes[3].set_title("Error")
 
+    plt.suptitle(textl)
     plt.tight_layout()
-    plt.savefig('figures/pattern_visualization.png')
+    plt.savefig('figures/'+name+'.png')
     plt.close()
+
+def curve_plot(loss_hist, name, textl):
+
+    plt.plot(loss_hist)
+    plt.xlabel("Epochs")
+    plt.ylabel("Van Rossum Loss")
+
+    plt.suptitle(textl)
+    plt.tight_layout()
+    plt.savefig('figures/'+name+'.png')
+    plt.close()
+
 
 
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--log-file", type=str, default="logs/test.csv", help='Log-file')
 parser.add_argument("--seed", type=int, default=80085, help='Random seed')
 
-parser.add_argument("--input", type=str, default="data/smile_data_set/input_700_250_25.pkl", help='Input pickle')
-parser.add_argument("--target", type=str, default="data/smile_data_set/smile95.pkl", help='Target pattern pickle')
-parser.add_argument("--architecture", type=str, default="700,400,250", help='Architecture of the networks')
+parser.add_argument("--data-set", type=str, default="NMNIST", help='Data set to use')
+parser.add_argument("--architecture", type=str, default="2048-500-100-10", help='Architecture of the networks')
 
-parser.add_argument("--l_rate", type=float, default=1e-6, help='Learning Rate')
-parser.add_argument("--epochs", type=int, default=20, help='Epochs')
+parser.add_argument("--l_rate", type=float, default=1e-5, help='Learning Rate')
+parser.add_argument("--epochs", type=int, default=2, help='Epochs')
 
-parser.add_argument("--alpha", type=float, default=.96, help='Time constant for membrane potential')
-parser.add_argument("--alpha_vr", type=float, default=.75, help='Time constant for Van Rossum distance')
+parser.add_argument("--w-scale", type=float, default=1, help='Weight Scaling')
+parser.add_argument("--batch-size", type=float, default=128, help='Batch Size ')
+
+parser.add_argument("--alpha", type=float, default=.6, help='Time constant for membrane potential')
+parser.add_argument("--gamma", type=float, default=.15, help='Reset Magnitude')
+parser.add_argument("--alpha_vr", type=float, default=.85, help='Time constant for Van Rossum distance')
 parser.add_argument("--thr", type=float, default=.1, help='Membrane Threshold')
 
 args = parser.parse_args()
@@ -96,57 +149,132 @@ key = random.PRNGKey(args.seed)
 model_uuid = str(uuid.uuid4())
 
 
-with open(args.input, 'rb') as f:
-    x_train = jnp.array(pickle.load(f)).transpose()
-with open(args.target, 'rb') as f:
-    y_train = jnp.array(pickle.load(f))
+if args.data_set == 'Smile':
+    if os.path.exists("data/smile_data_set/input_700_250_25.pkl") and os.path.exists("data/smile_data_set/smile95.pkl"):
+        with open("data/smile_data_set/input_700_250_25.pkl", 'rb') as f:
+            x_train = jnp.array(pickle.load(f)).transpose()
+            x_train = x_train.reshape((1, x_train.shape[0], x_train.shape[1]))
+        with open("data/smile_data_set/smile95.pkl", 'rb') as f:
+            y_train = jnp.array(pickle.load(f))
+            y_train = y_train.reshape((1, y_train.shape[0], y_train.shape[1]))
+    else:
+        raise Exception("Smile data set files do not exist please place them into data/smile_data_set")
+    train_dl = [(x_train, y_train)]
+    test_dl = [(x_train, y_train)]
+    loss_fn = vmap(vr_loss, (None, 0, 0))
+elif args.data_set == 'Yin_Yang':
+    pass
+elif args.data_set == 'NMNIST':
+    # 2* 32* 32 = 2048 and 10 classes
+    from torchneuromorphic.nmnist.nmnist_dataloaders import *
+    import torchneuromorphic.transforms as transforms
 
+    if os.path.exists('data/nmnist/n_mnist.hdf5'):
+        pass
+    elif os.path.exists('data/nmnist/'):
+        out = create_events_hdf5('data/nmnist', 'data/nmnist/n_mnist.hdf5')
+    else:
+        raise Exception("NMNIST data set does not exist, download and place raw data into data/nmnist")
+    
+    train_dl, test_dl = create_dataloader(
+            root='data/nmnist/n_mnist.hdf5',
+            batch_size=args.batch_size,
+            ds=1,
+            num_workers=4)
+    loss_fn = vmap(cla_loss, (None, 0, 0))
+elif args.data_set == 'DVS_Gestures':
+    from torchneuromorphic.dvs_gestures.dvsgestures_dataloaders import *
+    import torchneuromorphic.transforms as transforms
 
-layers = jnp.array(list(map(int, args.architecture.split(","))))
+    if os.path.exists('data/dvsgesture/dvs_gestures_build19.hdf5'):
+        pass
+    elif os.path.exists('data/dvsgesture/raw/'):
+        out = create_events_hdf5('data/dvsgesture/raw/', 'data/dvsgesture/dvs_gestures_build19.hdf5')
+    else:
+        raise Exception("DVS Gestures data set does not exist, download and place raw data into data/dvsgesture/raw/")
+    
+    train_dl, test_dl = create_dataloader(
+            root='data/dvsgesture/dvs_gestures_build19.hdf5',
+            batch_size=args.batch_size,
+            ds=1,
+            num_workers=4)
+else:
+    raise Exception("Unknown data set")
+
+layers = jnp.array(list(map(int, args.architecture.split("-"))))
 n_layers = len(layers)
 
 weights = []
 biases = []
 for i in range(len(layers)-1):
     key, subkey1 = random.split(key, 2)
-    weights.append(random.normal(subkey1, (layers[i+1], layers[i])) * jnp.sqrt(6/(layers[i] + layers[i+1])))
+    weights.append(random.normal(subkey1, (layers[i+1], layers[i])) * jnp.sqrt(6/(layers[i] + layers[i+1])) * args.w_scale)
     biases.append(jnp.zeros(layers[i+1]))
-
-
-def run_snn(weights, biases, alpha, thr, x_train):
-    T = x_train.shape[0]
-    mem = [jnp.zeros(l.shape[0]) for l in weights]
-    out_s = jnp.empty((0, weights[-1].shape[0]))
-
-    for t in range(T):
-        st = x_train[t,:]
-        for i, (w, b) in enumerate(zip(weights, biases)):
-            mem[i] = alpha * mem[i] + jnp.dot(weights[i], st) + biases[i]
-            st = spike_nonlinearity(mem[i], thr)
-            mem[i] -= st
-        out_s = jnp.vstack((out_s, st))
-    return out_s
-
-@jax.partial(jit, static_argnums=[2, 3, 4])
-def loss_pred(weights, biases, alpha, alpha_vr, thr, x_train, y_train):
-    pred_s = run_snn(weights, biases, alpha, thr, x_train)
-    return vr_loss(alpha_vr, pred_s, y_train)
 
 print(model_uuid)
 print(args)
 loss_hist = []
+train_hist = []
+test_hist = []
 for e in range(args.epochs):
-    loss, gwb = value_and_grad(loss_pred, argnums= (0,1))(weights, biases, args.alpha, args.alpha_vr, args.thr, x_train, y_train)
-    weights = [w - args.l_rate * gw.sum(0) for w, gw in zip(weights, gwb[0])]
-    biases = [b - args.l_rate * gb.sum(0) for b, gb in zip(biases, gwb[1])]
-    loss_hist.append(loss)
-    print("{} {:.4f}".format(e, loss))
+    acc_ta, acc_te, loss_r, s_te, s_ta = 0, 0, 0, 0, 0
+    for x_train, y_train in train_dl:
+        x_train = jnp.array(x_train.reshape((x_train.shape[0], x_train.shape[1], -1)))
+        y_train = jnp.array(y_train)
 
-pred = run_snn(weights, biases, args.alpha, args.thr, x_train)
-pattern_plot(x_train, y_train, pred)
+
+        loss, weights, biases = update_w(weights, biases, args.alpha, args.gamma, args.alpha_vr, args.thr, x_train, y_train, loss_fn)
+        pred = v_run_snn(weights, biases, args.alpha, args.gamma, args.thr, x_train)
+        
+        acc_ta = (acc_ta * s_ta + float(acc_compute(pred, y_train))  * int(x_train.shape[0])) / (s_ta + int(x_train.shape[0]))
+        loss_r = (loss_r * s_ta + loss * int(x_train.shape[0])) / (s_ta + int(x_train.shape[0]))
+        s_ta += int(x_train.shape[0])
+        #print("{} {:.4f} {:.4f}".format(s_ta, loss_r, acc_ta))
+
+    for x_test, y_test in test_dl:
+        x_test = jnp.array(x_test.reshape((x_test.shape[0], x_test.shape[1], -1)))
+        y_test = jnp.array(y_test)
+
+        pred = v_run_snn(weights, biases, args.alpha, args.gamma, args.thr, x_test)
+        
+        acc_te = (acc_te * s_te + float(acc_compute(pred, y_test)) * int(x_test.shape[0])) / (s_te + int(x_test.shape[0]))
+        s_te += int(x_test.shape[0])
+        #print("{} {:.4f}".format(s_te, acc_te))
+
+
+    loss_hist.append(loss_r)
+    train_hist.append(acc_ta)
+    test_hist.append(acc_te)
+    print("Epoch {} {:.4f} {:.4f} {:.4f}".format(e, loss_hist[-1], train_hist[-1], test_hist[-1]))
+
+# Visualization
+#pred = run_snn(weights, biases, args.alpha, args.gamma, args.thr, x_train)
+#pattern_plot(x_train, y_train, pred, model_uuid + "_pattern_visual", "")
 
 # save model
-jnp.savez("models/"+str(model_uuid)+".npz", weights, biases, loss_hist, args)
+jnp.savez("models/"+str(model_uuid)+".npz", weights = weights,  biases = biases, loss_hist = loss_hist, train_hist = train_hist, test_hist = test_hist,  args = args)
 # add log entry
 with open(args.log_file,'a') as f:
-    f.write(str(loss[-1]) + "," + ",".join( [str(vars(args)[t]) for t in vars(args)]))
+    f.write(str(loss_hist[-1]) + "," + str(train_hist[-1]) + "," + str(test_hist[-1]) + "," + model_uuid + "," + ",".join( [str(vars(args)[t]) for t in vars(args)]) + "\n")
+
+# # load model 
+# model_uuid = 'abfcaa0f-1de3-492a-a08e-5fcce8da513c'
+# npzfile = jnp.load("models/" + model_uuid + ".npz", allow_pickle=True)
+# weights = npzfile['arr_0']
+# biases = npzfile['arr_1']
+# loss_hist = npzfile['arr_2']
+# args = str(npzfile['arr_3'])[10:-1].split(',')
+# dargs = {}
+# for i in args:
+#     var, val = i.split("=")
+#     if "'" in val:
+#         dargs[var.strip(" ")] = val.strip("'")
+#     else:
+#         dargs[var.strip(" ")] = float(val)
+# args = argparse.Namespace(**dargs)
+
+# pred = run_snn(weights, biases, args.alpha, args.gamma, args.thr, x_train)
+# loss_v = vr_loss(args.alpha_vr, pred, y_train)
+# print(loss_v)
+# curve_plot(loss_hist, model_uuid + "_curve", str(loss_v) + " " + str(args.alpha_vr))
+# pattern_plot(x_train, y_train, pred, model_uuid + "_pattern_visual", str(loss_v) + " " + str(args.alpha_vr))
---
+++ grid_search.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+
+ident_word = "nmnist"
+
+part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q gpu@@joshi" 
+part11 = "\n#$ -l gpu_card=1\n#$ -N "
+
+part2 = "\n#$ -o ./logs/runs/output_"+ident_word+"_"
+
+part3 = ".txt\n#$ -e ./logs/runs/error_"+ident_word+"_"
+
+part4 = ".txt\nmodule load python cuda/10.2\nsetenv XLA_FLAGS --xla_gpu_cuda_data_dir=/afs/crc.nd.edu/x86_64_linux/c/cuda/10.2\nsetenv OMP_NUM_THREADS $NSLOTS\npython bptt_snn.py"
+
+
+random_seeds = [193012823 ,235899598, 8627169, 103372330, 14339038, 221706254, 46192121, 188833202, 37306063, 171928928]
+
+trials = 1
+
+# parameters = {
+# 'architecture' : ["700-400-250", "700-300-250", "700-500-250", "700-600-400-250"] ,
+# 'l_rate'       : [1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, .5, 1],
+# 'alpha'        : np.arange(.1,1.01,.05)  , 
+# 'gamma'        : np.arange(.1,1.01,.05)  ,      
+# 'alpha_vr'     : np.arange(.1,1.01,.05) ,         
+# 'thr'          : np.arange(.01,1.04,.05) 
+# }
+
+parameters = {
+'l_rate'       : [1e-7, 1e-5, 1e-3],
+'alpha'        : [.5, .6, .7, .8], 
+'gamma'        : [.1, .15, .2, .3],
+}
+
+parameters = {'l_rate':[1e-5]}
+
+
+for i in range(trials):
+    for variable in parameters:
+        for value in parameters[variable]:
+            name = ident_word + "_" +variable + "_" + str(value).replace(",","") + "_" + str(i)
+            with open('jobscripts/'+name+'.script', 'w') as f:
+                if isinstance(value, str):
+                    f.write(part1 + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + value+ "\" --data-set \"NMNIST\" --architecture \"2048-500-100-10\" --epochs 1 --seed " + str(random_seeds[i]) + " --log-file logs/" + ident_word + "_" + variable + ".csv") 
+                else:
+                    f.write(part1 + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " " + str(value)+ " --data-set \"NMNIST\" --architecture \"2048-500-100-10\" --epochs 1 --seed " + str(random_seeds[i]) + " --log-file logs/" + ident_word + "_" + variable + ".csv") 
+            os.system("qsub "+ 'jobscripts/'+name+'.script')
