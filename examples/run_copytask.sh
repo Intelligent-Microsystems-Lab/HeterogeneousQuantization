108db18a9d74e1afd891533704b6ccb7d5029488
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-04-21 23:47:08 -0400

    normal - run script fix

--- examples/run_copytask.sh
+++ examples/run_copytask.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 module load python cuda/10.2
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=/afs/crc.nd.edu/x86_64_linux/c/cuda/10.2"
-cd copytask_bptt
+cd copytask_bptt.py
 python train.py
-cd ../copytask_rtrl
+cd ../copytask_rtrl.py
 python train.py
-cd ../copytask_pc
+cd ../copytask_pc.py
 python train.py
 cd ..
--- examples/run_smile.sh
+++ examples/run_smile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 module load python cuda/10.2
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=/afs/crc.nd.edu/x86_64_linux/c/cuda/10.2"
-cd smile_bptt
+cd smile_bptt.py
 python train.py
-cd ../smile_rtrl
+cd ../smile_rtrl.py
 python train.py
-cd ../smile_pc
+cd ../smile_pc.py
 python train.py
 cd ..
