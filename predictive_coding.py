9d93111597f44eecef06ab603524a39b9f1b8d78
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-04-22 16:50:19 -0400

    normal - pc updates

--- predictive_coding.py
+++ predictive_coding.py
@@ -112,9 +112,6 @@ def compute_grads(params, input_seq, e_ys, e_hs, h_pred, mask=None):
             jnp.dot(input_seq[i].transpose(), (e_hs[i] * fn_deriv)) * mask[i]
         )
         dWh += jnp.dot(h_pred[i].transpose(), (e_hs[i] * fn_deriv)) * mask[i]
-        import pdb
-
-        pdb.set_trace()
 
     return {
         "cf": {"w1": dWx, "h1": dWh},
