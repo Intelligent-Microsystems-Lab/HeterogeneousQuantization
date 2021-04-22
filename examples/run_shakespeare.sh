9ae53d459134d424986262efc86c99c5057238c7
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-04-21 23:45:08 -0400

    normal - run script fix

--- examples/run_shakespeare.sh
+++ examples/run_shakespeare.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 module load python cuda/10.2
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=/afs/crc.nd.edu/x86_64_linux/c/cuda/10.2"
-cd shakespeare_bptt
+cd shakespeare_bptt.py
 python train.py
-cd ../shakespeare_rtrl
+cd ../shakespeare_rtrl.py
 python train.py
-cd ../shakespeare_pc
+cd ../shakespeare_pc.py
 python train.py
 cd ..
