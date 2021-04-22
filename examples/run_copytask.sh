ed65df826371f3ded48f7dd11b63761fb6a25028
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-04-22 11:52:39 -0400

    normal - para update

--- examples/run_copytask.sh
+++ examples/run_copytask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 module load python cuda/10.2
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=/afs/crc.nd.edu/x86_64_linux/c/cuda/10.2"
-cd copytask_pc.py
+cd copytask_pc
 python train.py
 cd ../copytask_rtrl.py
 python train.py
