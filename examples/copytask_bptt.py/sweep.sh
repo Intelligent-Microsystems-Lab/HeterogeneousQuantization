dc9ff358028808491cb6482933cdfb90e3b731dd
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-07-18 13:03:14 -0400

    normal - joshi sweep correction

--- examples/copytask_bptt.py/sweep.sh
+++ examples/copytask_bptt.py/sweep.sh
@@ -8,35 +8,35 @@ python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00_t5 --evaluatio
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t1 --evaluation_noise 0.0125 --seed 2029492581 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t2 --evaluation_noise 0.0125 --seed 2223210 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t3 --evaluation_noise 0.0125 --seed 1594305760 &
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t4 --evaluation_noise 0.0125 --seed & 87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t5 --evaluation_noise 0.0125 --seed & 2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t4 --evaluation_noise 0.0125 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00125_t5 --evaluation_noise 0.0125 --seed  2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t1 --evaluation_noise 0.025 --seed 2029492581 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t2 --evaluation_noise 0.025 --seed 2223210 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t3 --evaluation_noise 0.025 --seed 1594305760 &
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t4 --evaluation_noise 0.025 --seed & 87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t5 --evaluation_noise 0.025 --seed & 2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t4 --evaluation_noise 0.025 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0025_t5 --evaluation_noise 0.025 --seed  2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t1 --evaluation_noise 0.0375 --seed 2029492581 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t2 --evaluation_noise 0.0375 --seed 2223210 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t3 --evaluation_noise 0.0375 --seed 1594305760 &
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t4 --evaluation_noise 0.0375 --seed & 87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t5 --evaluation_noise 0.0375 --seed & 2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t4 --evaluation_noise 0.0375 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00375_t5 --evaluation_noise 0.0375 --seed  2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n005_t1 --evaluation_noise 0.05 --seed 2029492581 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n005_t2 --evaluation_noise 0.05 --seed 2223210 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n005_t3 --evaluation_noise 0.05 --seed 1594305760 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n005_t4 --evaluation_noise 0.05 --seed 87953651674304230 &
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n005_t5 --evaluation_noise 0.05 --seed & 2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n005_t5 --evaluation_noise 0.05 --seed 2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t1 --evaluation_noise 0.0625 --seed 2029492581 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t2 --evaluation_noise 0.0625 --seed 2223210 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t3 --evaluation_noise 0.0625 --seed 1594305760 &
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t4 --evaluation_noise 0.0625 --seed & 87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t5 --evaluation_noise 0.0625 --seed & 2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t4 --evaluation_noise 0.0625 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n00625_t5 --evaluation_noise 0.0625 --seed 2467475923055248755 & 
 
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t1 --evaluation_noise 0.075 --seed 2029492581 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t2 --evaluation_noise 0.075 --seed 2223210 &
 python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t3 --evaluation_noise 0.075 --seed 1594305760 &
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t4 --evaluation_noise 0.075 --seed & 87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t5 --evaluation_noise 0.075 --seed & 2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t4 --evaluation_noise 0.075 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/bptt_n0075_t5 --evaluation_noise 0.075 --seed 2467475923055248755 &
--- examples/copytask_pc/sweep.sh
+++ examples/copytask_pc/sweep.sh
@@ -8,20 +8,20 @@ python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00_t5 --evaluation_
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t1 --evaluation_noise 0.0125 --seed 2029492581 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t2 --evaluation_noise 0.0125 --seed 2223210 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t3 --evaluation_noise 0.0125 --seed 1594305760 & 
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t4 --evaluation_noise 0.0125 --seed &  87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t5 --evaluation_noise 0.0125 --seed &  2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t4 --evaluation_noise 0.0125 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00125_t5 --evaluation_noise 0.0125 --seed   2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0025_t1 --evaluation_noise 0.025 --seed 2029492581 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0025_t2 --evaluation_noise 0.025 --seed 2223210 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0025_t3 --evaluation_noise 0.025 --seed 1594305760 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0025_t4 --evaluation_noise 0.025 --seed 87953651674304230 & 
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0025_t5 --evaluation_noise 0.025 --seed &  2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0025_t5 --evaluation_noise 0.025 --seed 2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t1 --evaluation_noise 0.0375 --seed 2029492581 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t2 --evaluation_noise 0.0375 --seed 2223210 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t3 --evaluation_noise 0.0375 --seed 1594305760 & 
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t4 --evaluation_noise 0.0375 --seed &  87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t5 --evaluation_noise 0.0375 --seed &  2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t4 --evaluation_noise 0.0375 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00375_t5 --evaluation_noise 0.0375 --seed 2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n005_t1 --evaluation_noise 0.05 --seed 2029492581 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n005_t2 --evaluation_noise 0.05 --seed 2223210 & 
@@ -32,11 +32,11 @@ python3 train.py  --work_dir ../../../training_dir_sweep/pc_n005_t5 --evaluation
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t1 --evaluation_noise 0.0625 --seed 2029492581 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t2 --evaluation_noise 0.0625 --seed 2223210 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t3 --evaluation_noise 0.0625 --seed 1594305760 & 
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t4 --evaluation_noise 0.0625 --seed &  87953651674304230
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t5 --evaluation_noise 0.0625 --seed &  2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t4 --evaluation_noise 0.0625 --seed 87953651674304230 &
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n00625_t5 --evaluation_noise 0.0625 --seed 2467475923055248755 &
 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0075_t1 --evaluation_noise 0.075 --seed 2029492581 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0075_t2 --evaluation_noise 0.075 --seed 2223210 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0075_t3 --evaluation_noise 0.075 --seed 1594305760 & 
 python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0075_t4 --evaluation_noise 0.075 --seed 87953651674304230 & 
-python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0075_t5 --evaluation_noise 0.075 --seed &  2467475923055248755
+python3 train.py  --work_dir ../../../training_dir_sweep/pc_n0075_t5 --evaluation_noise 0.075 --seed 2467475923055248755 &
