e07f5938aac68f817fb6efd02c229e9535d808b1
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-09-14 13:39:16 -0400

    normal - run script fix

--- examples/cifar10_pc/run_noise_sweeps.sh
+++ examples/cifar10_pc/run_noise_sweeps.sh
@@ -2,7 +2,6 @@
 module load python
 
 # Several noise sweeps on Notre Dame CRC cluster.
-
 MAIN_DIR="/afs/crc.nd.edu/user/c/cschaef6/cifar10_noise_sweeps"
 USER="cschaef6"
 GPU_Q="gpu@@joshi"
@@ -19,7 +18,7 @@ python3 sweep.py --result_dir $MAIN_DIR/err_inpt_noise_pc --start $START --stop 
 python3 sweep.py  --result_dir $MAIN_DIR/err_weight_noise_pc --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter err_weight_noise --network_type pc
 
 # Backpropagation Sweeps
-python3 sweep.py --result_dir $MAIN_DIR/weight_noise_pc --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter weight_noise --network_type bp
-python3 sweep.py --result_dir $MAIN_DIR/act_noise_pc --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q  --parameter act_noise --network_type bp
-python3 sweep.py --result_dir $MAIN_DIR/err_inpt_noise_pc --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter err_inpt_noise --network_type bp
-python3 sweep.py  --result_dir $MAIN_DIR/err_weight_noise_pc --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter err_weight_noise --network_type bp
+python3 sweep.py --result_dir $MAIN_DIR/weight_noise_bp --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter weight_noise --network_type bp
+python3 sweep.py --result_dir $MAIN_DIR/act_noise_bp --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q  --parameter act_noise --network_type bp
+python3 sweep.py --result_dir $MAIN_DIR/err_inpt_noise_bp --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter err_inpt_noise --network_type bp
+python3 sweep.py  --result_dir $MAIN_DIR/err_weight_noise_bp --start $START --stop $STOP --step $STEP --user $USER --queue $GPU_Q --parameter err_weight_noise --network_type bp
