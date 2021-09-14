af0e8a4971dcdd963e88dcc7c75917c3c4dd0a81
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-09-14 13:33:52 -0400

    normal - run script fix

--- examples/cifar10_pc/run_noise_sweeps.sh
+++ examples/cifar10_pc/run_noise_sweeps.sh
@@ -7,7 +7,7 @@ MAIN_DIR="/afs/crc.nd.edu/user/c/cschaef6/cifar10_noise_sweeps"
 USER="cschaef6"
 GPU_Q="gpu@@joshi"
 START="0."
-END=".15"
+STOP=".15"
 STEP="0.001"
 
 mkdir /afs/crc.nd.edu/user/c/cschaef6/cifar10_noise_sweeps
