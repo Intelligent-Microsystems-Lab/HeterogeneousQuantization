71bbf4336e0804b5de41e80bc3e3baf0329ec09a
Clemens Schaefer <clemensschaefer1994@googlemail.com>
2021-09-19 10:13:28 -0400

    normal - figure script;

--- examples/mnist_pc/figures.py
+++ examples/mnist_pc/figures.py
@@ -172,87 +172,87 @@ def mean_std_eval_acc(path, samples):
 # plt.close()
 
 
-# samples = 1
-# mean_pc_weight, std_obs_weight, x_pc_weight = mean_std_eval_acc(
-#     bp_weight_noise, samples
-# )
-# mean_pc_act, std_obs_act, x_pc_act = mean_std_eval_acc(bp_act_noise, samples)
-# mean_pc_err_inpt, std_obs_err_inpt, x_pc_err_inpt = mean_std_eval_acc(
-#     bp_err_inpt_noise, samples
-# )
-# mean_pc_err_weight, std_obs_err_weight, x_pc_err_weight = mean_std_eval_acc(
-#     bp_err_weight_noise, samples
-# )
+samples = 1
+mean_pc_weight, std_obs_weight, x_pc_weight = mean_std_eval_acc(
+    bp_weight_noise, samples
+)
+mean_pc_act, std_obs_act, x_pc_act = mean_std_eval_acc(bp_act_noise, samples)
+mean_pc_err_inpt, std_obs_err_inpt, x_pc_err_inpt = mean_std_eval_acc(
+    bp_err_inpt_noise, samples
+)
+mean_pc_err_weight, std_obs_err_weight, x_pc_err_weight = mean_std_eval_acc(
+    bp_err_weight_noise, samples
+)
 
-# mean_pc_weight_bwd, std_obs_weight_bwd, x_pc_weight_bwd = mean_std_eval_acc(
-#     bp_weight_bwd_noise, samples
-# )
-# mean_pc_act_bwd, std_obs_act_bwd, x_pc_act_bwd = mean_std_eval_acc(
-#     bp_act_bwd_noise, samples
-# )
+mean_pc_weight_bwd, std_obs_weight_bwd, x_pc_weight_bwd = mean_std_eval_acc(
+    bp_weight_bwd_noise, samples
+)
+mean_pc_act_bwd, std_obs_act_bwd, x_pc_act_bwd = mean_std_eval_acc(
+    bp_act_bwd_noise, samples
+)
 
-# # Plot PC
+# Plot PC
 
-# fig, ax = plt.subplots(figsize=(8, 5.5))
-# ax.spines["top"].set_visible(False)
-# ax.spines["right"].set_visible(False)
+fig, ax = plt.subplots(figsize=(8, 5.5))
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
 
-# ax.plot(x_pc_weight, mean_pc_weight, label="Weight")
-# ax.fill_between(
-#     x_pc_weight,
-#     mean_pc_weight - std_obs_weight,
-#     mean_pc_weight + std_obs_weight,
-#     alpha=0.1,
-# )
+ax.plot(x_pc_weight, mean_pc_weight, label="Weight")
+ax.fill_between(
+    x_pc_weight,
+    mean_pc_weight - std_obs_weight,
+    mean_pc_weight + std_obs_weight,
+    alpha=0.1,
+)
 
-# ax.plot(x_pc_act, mean_pc_act, label="Activation")
-# ax.fill_between(
-#     x_pc_act, mean_pc_act - std_obs_act, mean_pc_act + std_obs_act, alpha=0.1
-# )
+ax.plot(x_pc_act, mean_pc_act, label="Activation")
+ax.fill_between(
+    x_pc_act, mean_pc_act - std_obs_act, mean_pc_act + std_obs_act, alpha=0.1
+)
 
-# ax.plot(x_pc_err_inpt, mean_pc_err_inpt, label="Error Activation")
-# ax.fill_between(
-#     x_pc_err_inpt,
-#     mean_pc_err_inpt - std_obs_err_inpt,
-#     mean_pc_err_inpt + std_obs_err_inpt,
-#     alpha=0.1,
-# )
+ax.plot(x_pc_err_inpt, mean_pc_err_inpt, label="Error Activation")
+ax.fill_between(
+    x_pc_err_inpt,
+    mean_pc_err_inpt - std_obs_err_inpt,
+    mean_pc_err_inpt + std_obs_err_inpt,
+    alpha=0.1,
+)
 
-# ax.plot(x_pc_err_weight, mean_pc_err_weight, label="Error Weight")
-# ax.fill_between(
-#     x_pc_err_weight,
-#     mean_pc_err_weight - std_obs_err_weight,
-#     mean_pc_err_weight + std_obs_err_weight,
-#     alpha=0.1,
-# )
-
-
-# ax.plot(x_pc_weight_bwd, mean_pc_weight_bwd, label="Weight BWD")
-# ax.fill_between(
-#     x_pc_weight_bwd,
-#     mean_pc_weight_bwd - std_obs_weight_bwd,
-#     mean_pc_weight_bwd + std_obs_weight_bwd,
-#     alpha=0.1,
-# )
-
-# ax.plot(x_pc_act_bwd, mean_pc_act_bwd, label="Activation BWD")
-# ax.fill_between(
-#     x_pc_act_bwd,
-#     mean_pc_act_bwd - std_obs_act_bwd,
-#     mean_pc_act_bwd + std_obs_act_bwd,
-#     alpha=0.1,
-# )
+ax.plot(x_pc_err_weight, mean_pc_err_weight, label="Error Weight")
+ax.fill_between(
+    x_pc_err_weight,
+    mean_pc_err_weight - std_obs_err_weight,
+    mean_pc_err_weight + std_obs_err_weight,
+    alpha=0.1,
+)
 
 
-# # ax.set_xscale('log')
-# # ax.set_yscale('log')
-# plt.legend(
-#     bbox_to_anchor=(0.5, 1.2), loc="upper center", ncol=2, frameon=False
-# )
-# plt.tight_layout()
-# plt.savefig("figures/bp_noise_ablation.png")
-# # plt.show()
-# plt.close()
+ax.plot(x_pc_weight_bwd, mean_pc_weight_bwd, label="Weight BWD")
+ax.fill_between(
+    x_pc_weight_bwd,
+    mean_pc_weight_bwd - std_obs_weight_bwd,
+    mean_pc_weight_bwd + std_obs_weight_bwd,
+    alpha=0.1,
+)
+
+ax.plot(x_pc_act_bwd, mean_pc_act_bwd, label="Activation BWD")
+ax.fill_between(
+    x_pc_act_bwd,
+    mean_pc_act_bwd - std_obs_act_bwd,
+    mean_pc_act_bwd + std_obs_act_bwd,
+    alpha=0.1,
+)
+
+
+# ax.set_xscale('log')
+# ax.set_yscale('log')
+plt.legend(
+    bbox_to_anchor=(0.5, 1.2), loc="upper center", ncol=2, frameon=False
+)
+plt.tight_layout()
+plt.savefig("figures/bp_noise_ablation.png")
+# plt.show()
+plt.close()
 
 
 samples = 1
