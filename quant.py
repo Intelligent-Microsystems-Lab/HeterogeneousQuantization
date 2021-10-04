982d65831d10d681d6db49e97080da2238ac863c
clee1994 <cschaef6@nd.edu>
2021-10-04 20:13:46 +0000

    normal - quant start

--- quant.py
+++ quant.py
@@ -8,6 +8,14 @@ Array = Any
 PRNGKey = Any
 
 
+def get_noise(x: Array, percentage: float, rng: PRNGKey) -> Array:
+  return (
+      jnp.max(jnp.abs(x))
+      * percentage
+      * jax.random.uniform(rng, x.shape, minval=-1, maxval=1.0)
+  )
+
+
 def signed_uniform_max_scale_quant_ste(x: Array, bits: int) -> Array:
   if type(bits) == int:
     assert (
@@ -27,9 +35,44 @@ def signed_uniform_max_scale_quant_ste(x: Array, bits: int) -> Array:
   return x - jax.lax.stop_gradient(x - xq)
 
 
-def get_noise(x: Array, percentage: float, rng: PRNGKey) -> Array:
-  return (
-      jnp.max(jnp.abs(x))
-      * percentage
-      * jax.random.uniform(rng, x.shape, minval=-1, maxval=1.0)
-  )
+# parametric homogenouse quantization
+class parametric_d(nn.Module):
+  # Based on LEARNED STEP SIZE QUANTIZATION
+  # https://arxiv.org/abs/1902.08153.
+  @nn.compact
+  def __call__(self, inputs: Array,) -> Array:
+    pass
+
+# parametric heterogenous quantization
+
+
+class parametric_quant_d_xmax(nn.Module):
+  # Based on MIXED PRECISION DNNS
+  # https://openreview.net/pdf?id=Hyx0slrFvH
+  @nn.compact
+  def __call__(self, inputs: Array,) -> Array:
+    pass
+
+# differentiable quantization
+
+
+def differentiable_quant(x: Array, bits: int) -> Array:
+  # Based on Differentiable Soft Quantization
+  # https://arxiv.org/abs/1908.05033
+  pass
+
+# parametric homogenouse differentiable quantization
+
+
+class parametric_differentiable_d(nn.Module):
+  @nn.compact
+  def __call__(self, inputs: Array,) -> Array:
+    pass
+
+# parametric heterogenous differentiable quantization
+
+
+class parametric_differentiable_d_xmax(nn.Module):
+  @nn.compact
+  def __call__(self, inputs: Array,) -> Array:
+    pass
