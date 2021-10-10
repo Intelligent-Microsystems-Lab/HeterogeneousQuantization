f2b0d56576e8e4e5e08c50ebac4813af54eda781
Clemens Schaefer <15638937+clee1994@users.noreply.github.com>
2021-10-10 16:15:31 -0400

    normal - config fix

--- examples/resnet/configs/default.py
+++ examples/resnet/configs/default.py
@@ -27,6 +27,8 @@ def get_config():
   config.cache = True
   config.half_precision = False
 
+  config.pretrained = '../..'
+
   # If num_train_steps==-1 then the number of training steps is calculated from
   # num_epochs using the entire dataset. Similarly for steps_per_eval.
   config.num_train_steps = -1
