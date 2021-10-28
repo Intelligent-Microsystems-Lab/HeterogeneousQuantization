normal - corrected sign, 73.92 on enet0 int8 no training

examples/efficientnet/models.py
@@ -136,16 +136,10 @@ class MBConvBlock(nn.Module):
                     use_bias=False,
                     config=self.config,
                     bits=self.bits,
-                    quant_act_sign=(self.block_num == 0))(x)
+                    quant_act_sign=True)(x)
       x = self.norm()(x)
       x = self.act(x)
-      expand_bool = True
       logging.info('Expand shape: %s', x.shape)
-    else:
-      if self.block_num == 0:
-        expand_bool = True
-      else:
-        expand_bool = False
 
     # Depthwise convolution
     kernel_size = self.kernel_size
@@ -163,7 +157,7 @@ class MBConvBlock(nn.Module):
         name='depthwise_conv2d',
         config=self.config,
         bits=self.bits,
-        quant_act_sign=not expand_bool)(x)
+        quant_act_sign=True)(x)
     x = self.norm()(x)
     x = self.act(x)
     logging.info('DWConv shape: %s', x.shape)
@@ -265,8 +259,7 @@ class EfficientNet(nn.Module):
         use_bias=False,
         name='stem_conv',
         config=self.config.quant.stem,
-        bits=self.config.quant.bits,
-        quant_act_sign=False)(x)
+        bits=self.config.quant.bits)(x)
 
     x = norm(name='stem_bn')(x)
     x = self.act(x)

examples/efficientnet/visualization.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import tensorboard as tb
 from packaging import version
+import grpc
 
 import numpy as np
+import pandas as pd
 
 # Validation Runs.
 
@@ -214,228 +216,286 @@ enet0_dynamic_init_double_cos_ewgs = {
 
 
 enets_fp32_t2 = {
-  4652008: 'oIBswEoqTme2oLlkkhx4Xw',
-  5416680: 'lHDJHMe2RamzzWwF4DF1hQ',
-  6092072: 'RiESL4q5QGuzCO66fYZ70Q',
-  8197096: 'LvwDTyJbRwKlvE77mBtWhw',
-  13006568: 'rdXGZbqPTyuOem2n2s7K0Q',
-  'params': 32,
-  'label': 'EfficientNet-Lites (FP32)',
+    4652008: 'oIBswEoqTme2oLlkkhx4Xw',
+    5416680: 'lHDJHMe2RamzzWwF4DF1hQ',
+    6092072: 'RiESL4q5QGuzCO66fYZ70Q',
+    8197096: 'LvwDTyJbRwKlvE77mBtWhw',
+    13006568: 'rdXGZbqPTyuOem2n2s7K0Q',
+    'params': 32,
+    'label': 'EfficientNet-Lites (FP32)',
 }
 
 
 enets_int8_t2 = {
-  4652008: 'NAxjD6ydQDq25NdJ7RQauA',
-  5416680: 'b1kq2yDsRrSoQORKAoiwHQ',
-  6092072: 'PpcD8wrzRga4S5Qg3RxKfg',
-  8197096: 'EyXpX0NbTnqvQt88F9JdSg',
-  13006568: 'ZjKyJ1nlSqqtGivBytyVdg',
-  'params': 8,
-  'label': 'EfficientNet-Lites (INT8)',
+    4652008: 'NAxjD6ydQDq25NdJ7RQauA',
+    5416680: 'b1kq2yDsRrSoQORKAoiwHQ',
+    6092072: 'PpcD8wrzRga4S5Qg3RxKfg',
+    8197096: 'EyXpX0NbTnqvQt88F9JdSg',
+    13006568: 'ZjKyJ1nlSqqtGivBytyVdg',
+    'params': 8,
+    'label': 'EfficientNet-Lites (INT8)',
 }
-
 
 
 enet0_dynamic_init_double_mean_t2 = {
-  2: 'GHq9K5XuQNyxqD6GGlcPQg',
-  3: 'brXMuvyRS1W22FR24KaCXA',
-  4: '8gGvrbG6RC2YqiRfQzXANw',
-  5: 'v6hkFRfbQA6Tbw3aTPRmAA',
-  6: 'DvKtwOR6Tl6ONeN879JQbQ',
-  7: 'mPkKzrxrR2yWf9koRDCaUw',
-  8: '21cNoDLSQ9GTwvSw0da43g',
-  'params': 4652008,
-  'label': 'EfficientNet-Lite0 Dynamic Quant Init Double Mean',
+    2: 'GHq9K5XuQNyxqD6GGlcPQg',
+    3: 'brXMuvyRS1W22FR24KaCXA',
+    4: '8gGvrbG6RC2YqiRfQzXANw',
+    5: 'v6hkFRfbQA6Tbw3aTPRmAA',
+    6: 'DvKtwOR6Tl6ONeN879JQbQ',
+    7: 'mPkKzrxrR2yWf9koRDCaUw',
+    8: '21cNoDLSQ9GTwvSw0da43g',
+    'params': 4652008,
+    'label': 'EfficientNet-Lite0 Dynamic Quant Init Double Mean',
 }
 
 enet0_static_init_double_mean_t2 = {
-  2: 'J4NdnC9XRzOQnkymDBAjxA',
-  3: 'Xo9GWJ1BQbqjF6x3WJjvMg',
-  4: 'f9clPhE3QHeAJH8nvrSJqQ',
-  5: 'LVXphdloRXSR0FmhZLwDxg',
-  6: 'asOKaXvkTzKBuQhk89E0CQ',
-  7: 'xqQR8WZsQBGJTGXc42qgtA',
-  8: 'bG0YRHwQSOKpYyNroDiYRA',
-  'params': 4652008,
-  'label': 'EfficientNet-Lite0 Static Quant Init Double Mean',
+    2: 'J4NdnC9XRzOQnkymDBAjxA',
+    3: 'Xo9GWJ1BQbqjF6x3WJjvMg',
+    4: 'f9clPhE3QHeAJH8nvrSJqQ',
+    5: 'LVXphdloRXSR0FmhZLwDxg',
+    6: 'asOKaXvkTzKBuQhk89E0CQ',
+    7: 'xqQR8WZsQBGJTGXc42qgtA',
+    8: 'bG0YRHwQSOKpYyNroDiYRA',
+    'params': 4652008,
+    'label': 'EfficientNet-Lite0 Static Quant Init Double Mean',
 }
 
 enet0_dynamic_init_gaussian_t2 = {
-  2: 'NbEovMSJRpmWQUN7kxAN9Q',
-  3: '0VHp8egmSXGcEMrJwHJUrQ',
-  4: 'RJldVC0UQim8fYoJJj9rUg',
-  5: 'OCj3BknTRyWvAGVT6d2PLA',
-  6: 'mZkrNxWRSo6jazfdJQjxOg',
-  7: 'pP5neKFJRa6LQWYeGigR2w',
-  8: 'rXW4RcMxQpOE64YKknZsDQ',
-  'params': 4652008,
-  'label': 'EfficientNet-Lite0 Dynamic Quant Init Gaussian',
+    2: 'NbEovMSJRpmWQUN7kxAN9Q',
+    3: '0VHp8egmSXGcEMrJwHJUrQ',
+    4: 'RJldVC0UQim8fYoJJj9rUg',
+    5: 'OCj3BknTRyWvAGVT6d2PLA',
+    6: 'mZkrNxWRSo6jazfdJQjxOg',
+    7: 'pP5neKFJRa6LQWYeGigR2w',
+    8: 'rXW4RcMxQpOE64YKknZsDQ',
+    'params': 4652008,
+    'label': 'EfficientNet-Lite0 Dynamic Quant Init Gaussian',
 }
 
 enet0_static_init_gaussian_t2 = {
-  2: 'XnQ2t7qxR9yFrLtlkbWONQ',
-  3: 'Sv5NEXHGQ5qG21llbjUa0g',
-  4: 'cWEEkRk2T2uxEsRtdPUPyA',
-  5: '8sY3PAc7Qo6WMKaWg4qzyw',
-  6: '6RkmMxdeRGO6BPED6jljAw',
-  7: 'Vslt1nD5S9i0eCz18rOuVA',
-  8: '4xIj338JT92W5kxZ9f4MGQ',
-  'params': 4652008,
-  'label': 'EfficientNet-Lite0 Static Quant Init Gaussian',
+    2: 'XnQ2t7qxR9yFrLtlkbWONQ',
+    3: 'Sv5NEXHGQ5qG21llbjUa0g',
+    4: 'cWEEkRk2T2uxEsRtdPUPyA',
+    5: '8sY3PAc7Qo6WMKaWg4qzyw',
+    6: '6RkmMxdeRGO6BPED6jljAw',
+    7: 'Vslt1nD5S9i0eCz18rOuVA',
+    8: '4xIj338JT92W5kxZ9f4MGQ',
+    'params': 4652008,
+    'label': 'EfficientNet-Lite0 Static Quant Init Gaussian',
 }
 
 enet0_mixed = {
-  0: 's8XTz88OSw6Fg8YJtq2yog',
-  'label': 'EfficientNet-Lite0 Mixed Precision',
+    0: 's8XTz88OSw6Fg8YJtq2yog',
+    'label': 'EfficientNet-Lite0 Mixed Precision',
 }
+
+
+enet0_dynamic_lr = {
+    '2_0.0001': 'UZOSDddESQyHddq4Pd5h5g',
+    '2_0.001': '4Vca3zP4Q1ehmybBmYOnjA',
+    '2_0.01': 'LdnrLmPOTli4ARoPko2vng',
+    '3_0.0001': '7aMxGAdFQjijeY72PAHJug',
+    '3_0.001': 'IvVuZwtDRk6j6K8Y9wyAQQ',
+    '3_0.01': 'ridDAAXrSM20JgYKt1In2A',
+    '4_0.0001': 'CaVrGLq5S4CA8D4Eo0Xt8w',
+    '4_0.001': '1wEV3pr9TKmekCe5GzIGaw',
+    '4_0.01': '1Rv0OgSNRoqFBXhZMLS6yQ',
+    '5_0.0001': 'EmQmTDLxRzKpjV0h6mnc2Q',
+    '5_0.001': 'rSR6fDBLSYyGQXvrfXiowA',
+    '5_0.01': 'FpErwWBURQWjhLbzvN9WEw',
+    '6_0.0001': 'oOVM6d6BTGqdyz9ZcnBDmQ',
+    '6_0.001': 'ZwUPJar0S82sYgKzYU7p8w',
+    '6_0.01': 'KcBTvFXdRaGXsvHJgP3EmQ',
+    '7_0.0001': 'Rh7ED2lVRO2Dxo4S82tkRQ',
+    '7_0.001': 'ge0gpVljTaqFekClKnbNxw',
+    '7_0.01': 'KTXEibNrR9GbbBRdLqnwBw',
+    '8_0.0001': '',
+    '8_0.001': '',
+    '8_0.01': '',
+}
+
+
+enet0_static_lr = {
+    '2_0.0001': 'N2B02OFrQxG5FdBnQlPV4w',
+    '2_0.001': '1FPHpwNtR1WyeOAERB7VSw',
+    '2_0.01': 'Ou4bT5RhQ1K7UjGhGLMCRA',
+    '3_0.0001': 'HKO2RO3zT0upPQbhaiVkcw',
+    '3_0.001': 'GxhbeLQnRbaWCMm6XFJ4Mw',
+    '3_0.01': 'xNuwHACwQSWj3yc98K9kSw',
+    '4_0.0001': 'nsP3bqOFQT65WGGx5KuRDg',
+    '4_0.001': 'pdv9vTnLSruAZLhOH1mD0A',
+    '4_0.01': 'saCgSw2YQlWJUrPlvUDQGw',
+    '5_0.0001': 'pXAa3dUwR8yMTGRaWorlxA',
+    '5_0.001': 'HnAmfQxQSySbgwPg6t8WHw',
+    '5_0.01': 'jxho21EyQ1e7Lpxf73ZfDg',
+    '6_0.0001': 'YR48o7ZtRX2N7KxOOV6HDw',
+    '6_0.001': 'yABDLx4tQcq6Cqh49rQoow',
+    '6_0.01': 'bEdgxp4WTKeQdzkcZuq6ZA',
+    '7_0.0001': 'xjmoVj4SQjyd4dhv8zN6lQ',
+    '7_0.001': 'BdwmgzNLQQWjHmbsntbWZw',
+    '7_0.01': 'Mou0mCwMTB6v67lZv2reJw',
+    '8_0.0001': '',
+    '8_0.001': '',
+    '8_0.01': '',
+}
+
 
 # Competitor Performance.
 
 competitors = {
-  'pact_resnet18' : {
-      # https://arxiv.org/pdf/1805.06085.pdf
-      'eval_err': [1 - 0.644, 1 - 0.681, 1 - 0.692, 1 - 0.698],
-      'size_mb': np.array([2, 3, 4, 5]) * 11679912 / 8_000_000,
-      'name': 'PACT ResNet18',
-      'alpha':.15,
-      # no first and last layer quant
-  },
+    'pact_resnet18': {
+        # https://arxiv.org/pdf/1805.06085.pdf
+        'eval_err': [1 - 0.644, 1 - 0.681, 1 - 0.692, 1 - 0.698],
+        'size_mb': np.array([2, 3, 4, 5]) * 11679912 / 8_000_000,
+        'name': 'PACT ResNet18',
+        'alpha': .15,
+        # no first and last layer quant
+    },
 
-  'pact_resnet50' : {
-      # https://arxiv.org/pdf/1805.06085.pdf
-      'eval_err': [1 - 0.722, 1 - 0.753, 1 - 0.765, 1 - 0.767],
-      'size_mb': np.array([2, 3, 4, 5]) * 25636712 / 8_000_000,
-      'name': 'PACT ResNet50',
-      'alpha':1.,
-      # no first and last layer quant
-  },
+    'pact_resnet50': {
+        # https://arxiv.org/pdf/1805.06085.pdf
+        'eval_err': [1 - 0.722, 1 - 0.753, 1 - 0.765, 1 - 0.767],
+        'size_mb': np.array([2, 3, 4, 5]) * 25636712 / 8_000_000,
+        'name': 'PACT ResNet50',
+        'alpha': 1.,
+        # no first and last layer quant
+    },
 
-  'pact_mobilev2' : {
-      # https://arxiv.org/pdf/1811.08886.pdf
-      'eval_err': [1 - 0.6139, 1 - 0.6884, 1 - 0.7125],
-      'size_mb': np.array([4, 5, 6]) * 3300000  / 8_000_000,
-      'name': 'PACT MobileNetV2',
-      'alpha':.15,
-  },
+    'pact_mobilev2': {
+        # https://arxiv.org/pdf/1811.08886.pdf
+        'eval_err': [1 - 0.6139, 1 - 0.6884, 1 - 0.7125],
+        'size_mb': np.array([4, 5, 6]) * 3300000 / 8_000_000,
+        'name': 'PACT MobileNetV2',
+        'alpha': .15,
+    },
 
-  'dsq_resnet18' : {
-      # https://arxiv.org/abs/1908.05033
-      'eval_err': [1 - 0.6517, 1 - 0.6866, 1 - 0.6956],
-      'size_mb': np.array([2, 3, 4]) * 11679912  / 8_000_000,
-      'name': 'DSQ ResNet18',
-      'alpha':.15,
-  },
+    'dsq_resnet18': {
+        # https://arxiv.org/abs/1908.05033
+        'eval_err': [1 - 0.6517, 1 - 0.6866, 1 - 0.6956],
+        'size_mb': np.array([2, 3, 4]) * 11679912 / 8_000_000,
+        'name': 'DSQ ResNet18',
+        'alpha': .15,
+    },
 
-  'lsq_resnet18' : {
-      # https://arxiv.org/abs/1902.08153
-      'eval_err': [1 - 0.676, 1 - 0.702, 1 - 0.711, 1 - 0.711],
-      'size_mb': np.array([2, 3, 4, 8]) * 11679912  / 8_000_000,
-      'name': 'LSQ ResNet18',
-      'alpha':.15,
-  },
+    'lsq_resnet18': {
+        # https://arxiv.org/abs/1902.08153
+        'eval_err': [1 - 0.676, 1 - 0.702, 1 - 0.711, 1 - 0.711],
+        'size_mb': np.array([2, 3, 4, 8]) * 11679912 / 8_000_000,
+        'name': 'LSQ ResNet18',
+        'alpha': .15,
+    },
 
-  'lsqp_resnet18' : {
-      # https://arxiv.org/abs/2004.09576
-      'eval_err': [1 - 0.668, 1 - 0.694, 1 - 0.708],
-      'size_mb': np.array([2, 3, 4]) * 11679912  / 8_000_000,
-      'name': 'LSQ+ ResNet18',
-      'alpha':.15,
-  },
+    'lsqp_resnet18': {
+        # https://arxiv.org/abs/2004.09576
+        'eval_err': [1 - 0.668, 1 - 0.694, 1 - 0.708],
+        'size_mb': np.array([2, 3, 4]) * 11679912 / 8_000_000,
+        'name': 'LSQ+ ResNet18',
+        'alpha': .15,
+    },
 
-  'lsqp_enet0' : {
-      # https://arxiv.org/abs/2004.09576
-      'eval_err': [1 - 0.491, 1 - 0.699, 1 - 0.738],
-      'size_mb': np.array([2, 3, 4]) * 5330571  / 8_000_000, # number might be incorrect
-      'name': 'LSQ+ EfficientNet-B0',
-      'alpha':1.,
-  },
+    'lsqp_enet0': {
+        # https://arxiv.org/abs/2004.09576
+        'eval_err': [1 - 0.491, 1 - 0.699, 1 - 0.738],
+        # number might be incorrect
+        'size_mb': np.array([2, 3, 4]) * 5330571 / 8_000_000,
+        'name': 'LSQ+ EfficientNet-B0',
+        'alpha': 1.,
+    },
 
-  'ewgs_resnet18' : {
-      # https://arxiv.org/abs/2104.00903
-      'eval_err': [1 - 0.553, 1 - 0.67, 1 - 0.697, 1 - 0.706],
-      'size_mb': np.array([1, 2, 3, 4]) * 11679912  / 8_000_000,
-      'name': 'EWGS ResNet18',
-      'alpha':.15,
-  },
+    'ewgs_resnet18': {
+        # https://arxiv.org/abs/2104.00903
+        'eval_err': [1 - 0.553, 1 - 0.67, 1 - 0.697, 1 - 0.706],
+        'size_mb': np.array([1, 2, 3, 4]) * 11679912 / 8_000_000,
+        'name': 'EWGS ResNet18',
+        'alpha': .15,
+    },
 
-  'ewgs_resnet34' : {
-      # https://arxiv.org/abs/2104.00903
-      'eval_err': [1 - 0.615, 1 - 0.714, 1 - 0.733, 1 - 0.739],
-      'size_mb': np.array([1, 2, 3, 4]) *  25557032 / 8_000_000,
-      'name': 'EWGS ResNet34',
-      'alpha':.15,
-  },
+    'ewgs_resnet34': {
+        # https://arxiv.org/abs/2104.00903
+        'eval_err': [1 - 0.615, 1 - 0.714, 1 - 0.733, 1 - 0.739],
+        'size_mb': np.array([1, 2, 3, 4]) * 25557032 / 8_000_000,
+        'name': 'EWGS ResNet34',
+        'alpha': .15,
+    },
 
-  'qil_resnet18' : {
-      # https://arxiv.org/abs/1808.05779
-      'eval_err': [1 - 0.704, 1 - 0.701, 1 - 0.692, 1 - 0.657],
-      'size_mb': np.array([5, 4, 3, 1]) *  11679912 / 8_000_000,
-      'name': 'QIL ResNet18',
-      'alpha':.15,
-      # no first and last layer quant
-  },
+    'qil_resnet18': {
+        # https://arxiv.org/abs/1808.05779
+        'eval_err': [1 - 0.704, 1 - 0.701, 1 - 0.692, 1 - 0.657],
+        'size_mb': np.array([5, 4, 3, 1]) * 11679912 / 8_000_000,
+        'name': 'QIL ResNet18',
+        'alpha': .15,
+        # no first and last layer quant
+    },
 
-  'qil_resnet34' : {
-      # https://arxiv.org/abs/1808.05779
-      'eval_err': [1 - 0.737, 1 - 0.737, 1 - 0.731, 1 - 0.706],
-      'size_mb': np.array([5, 4, 3, 1]) * 25557032  / 8_000_000,
-      'name': 'QIL ResNet34',
-      'alpha':.15,
-      # no first and last layer quant
-  },
+    'qil_resnet34': {
+        # https://arxiv.org/abs/1808.05779
+        'eval_err': [1 - 0.737, 1 - 0.737, 1 - 0.731, 1 - 0.706],
+        'size_mb': np.array([5, 4, 3, 1]) * 25557032 / 8_000_000,
+        'name': 'QIL ResNet34',
+        'alpha': .15,
+        # no first and last layer quant
+    },
 
-  'hawqv2_squeeze' : {
-      # https://arxiv.org/abs/1911.03852
-      'eval_err': [1 - 0.6838],
-      'size_mb': np.array([1.07]),
-      'name': 'HAWQ-V2 SqueezeNext',
-      'alpha':1.,
-  },
+    'hawqv2_squeeze': {
+        # https://arxiv.org/abs/1911.03852
+        'eval_err': [1 - 0.6838],
+        'size_mb': np.array([1.07]),
+        'name': 'HAWQ-V2 SqueezeNext',
+        'alpha': 1.,
+    },
 
-  'hawqv2_inceptionv3' : {
-      # https://arxiv.org/abs/1911.03852
-      'eval_err': [1 - 0.7568],
-      'size_mb': np.array([7.57]),
-      'name': 'HAWQ-V2 Inception-V3',
-      'alpha':1.,
-  },
+    'hawqv2_inceptionv3': {
+        # https://arxiv.org/abs/1911.03852
+        'eval_err': [1 - 0.7568],
+        'size_mb': np.array([7.57]),
+        'name': 'HAWQ-V2 Inception-V3',
+        'alpha': 1.,
+    },
 
-  'mixed_resnet18' : {
-      # https://arxiv.org/abs/1905.11452
-      'eval_err': [0.2992],
-      'size_mb': np.array([5.4]),
-      'name': 'Mixed Precision DNNs ResNet18',
-      'alpha':.15,
-  },
+    'mixed_resnet18': {
+        # https://arxiv.org/abs/1905.11452
+        'eval_err': [0.2992],
+        'size_mb': np.array([5.4]),
+        'name': 'Mixed Precision DNNs ResNet18',
+        'alpha': .15,
+    },
 
-  'mixed_mobilev2' : {
-      # https://arxiv.org/abs/1905.11452
-      'eval_err': [0.3026],
-      'size_mb': np.array([1.55]),
-      'name': 'Mixed Precision DNNs MobileNetV2',
-      'alpha':.15,
-  },
+    'mixed_mobilev2': {
+        # https://arxiv.org/abs/1905.11452
+        'eval_err': [0.3026],
+        'size_mb': np.array([1.55]),
+        'name': 'Mixed Precision DNNs MobileNetV2',
+        'alpha': .15,
+    },
 
-  'haq_mobilev2' : {
-      # https://arxiv.org/pdf/1811.08886.pdf
-      'eval_err': [1 - 0.6675, 1 - 0.7090, 1 - 0.7147],
-      'size_mb': np.array([.95, 1.38, 1.79]),
-      'name': 'HAQ MobileNetV2',
-      'alpha':1.,
-  },
+    'haq_mobilev2': {
+        # https://arxiv.org/pdf/1811.08886.pdf
+        'eval_err': [1 - 0.6675, 1 - 0.7090, 1 - 0.7147],
+        'size_mb': np.array([.95, 1.38, 1.79]),
+        'name': 'HAQ MobileNetV2',
+        'alpha': 1.,
+    },
 
-  'haq_resnet50' : {
-      # https://arxiv.org/pdf/1811.08886.pdf
-      'eval_err': [1 - 0.7063, 1 - 0.7530, 1 - 0.7614],
-      'size_mb': np.array([6.30, 9.22, 12.14]),
-      'name': 'HAQ ResNet50',
-      'alpha':1.,
-  },
+    'haq_resnet50': {
+        # https://arxiv.org/pdf/1811.08886.pdf
+        'eval_err': [1 - 0.7063, 1 - 0.7530, 1 - 0.7614],
+        'size_mb': np.array([6.30, 9.22, 12.14]),
+        'name': 'HAQ ResNet50',
+        'alpha': 1.,
+    },
 }
+
 
 def get_best_eval(experiment_id):
   experiment = tb.data.experimental.ExperimentFromDev(experiment_id)
-  df = experiment.get_scalars()
+
+  try:
+    df = experiment.get_scalars()
+  except grpc.RpcError as rpc_error:
+    print('Couldn\'t fetch experiment: ' + experiment_id + ' got error: '
+          + str(rpc_error))
+    return None
 
   data = df[df['run'] == 'eval']
   return data[data['tag'] == 'accuracy']['value'].max()
@@ -447,31 +507,38 @@ def get_best_eval_and_size(experiment_id):
 
   data = df[df['run'] == 'eval']
   max_eval = data[data['tag'] == 'accuracy']['value'].max()
-  vals_at_step = data[data['step'] ==  int(data[data['value'] == max_eval]['step'])]
+  vals_at_step = data[data['step'] == int(
+      data[data['value'] == max_eval]['step'])]
   size_mb = float(vals_at_step[vals_at_step['tag'] == 'weight_size']['value'])
   return max_eval, size_mb
 
 
+def table(res_dict):
+  df = pd.DataFrame({'0.01': [None, None, None, None, None, None, None],
+                     '0.001': [None, None, None, None, None, None, None],
+                     '0.0001': [None, None, None, None, None, None, None], })
+  df.rename(index={0: '2', 1: '3', 2: '4', 3: '5',
+            4: '6', 5: '7', 6: '8'}, inplace=True)
+  for k, v in res_dict.items():
+    acc = get_best_eval(v)
+    bit, lr = k.split('_')
+    df[lr][bit] = acc
+
+  return df
+
+
 def plot_line(ax, res_dict):
-  linestyle = 'solid'
   x = []
   y = []
   for key, value in res_dict.items():
     if key == 'label':
       label = value
-    elif key == 'color':
-      color = value
-    elif key == 'linestyle':
-      linestyle = value
     elif key == 'params':
-      num_params = value
+      pass
     else:
-      y.append(1-get_best_eval(value))
+      y.append(1 - get_best_eval(value))
       x.append(key * res_dict['params'] / 8_000_000)
 
-  #print(label)
-  #print(x)
-  #print(y)
   ax.plot(x, y, marker='x', label=label, ms=20, markeredgewidth=5, linewidth=5)
 
 
@@ -483,10 +550,11 @@ def plot_mixed(ax, res_dict):
       label = value
     else:
       acc_t, size_t = get_best_eval_and_size(value)
-      y.append(1-acc_t)
+      y.append(1 - acc_t)
       x.append(size_t)
 
   ax.plot(x, y, marker='x', label=label, ms=20, markeredgewidth=5, linewidth=5)
+
 
 def plot_comparison(name):
   font_size = 26
@@ -508,19 +576,20 @@ def plot_comparison(name):
 
   # Competitors.
   for competitor_name, competitor_data in competitors.items():
-    ax.plot(competitor_data['size_mb'], competitor_data['eval_err'], label = competitor_data['name'], marker='x', ms=20, markeredgewidth=5, linewidth=5, alpha=competitor_data['alpha'], linestyle='--')
+    ax.plot(competitor_data['size_mb'], competitor_data['eval_err'],
+            label=competitor_data['name'],
+            marker='x', ms=20, markeredgewidth=5, linewidth=5,
+            alpha=competitor_data['alpha'], linestyle='--')
 
   # Our own.
   # plot_line(ax, enets_fp32_t2)
   plot_line(ax, enets_int8_t2)
-
 
   plot_line(ax, enet0_dynamic_init_double_mean_t2)
   plot_line(ax, enet0_static_init_double_mean_t2)
   plot_line(ax, enet0_dynamic_init_gaussian_t2)
   plot_line(ax, enet0_static_init_gaussian_t2)
   plot_mixed(ax, enet0_mixed)
-
 
   ax.set_xlabel("Network Size (MB)", fontsize=font_size, fontweight='bold')
   ax.set_ylabel("Eval Error (%)", fontsize=font_size, fontweight='bold')
@@ -578,6 +647,11 @@ if __name__ == '__main__':
   print("TensorBoard version: ", tb.__version__)
 
   plot_comparison('figures/overview.png')
+
+  df_dynamic_lr = table(enet0_dynamic_lr)
+  df_static_lr = table(enet0_static_lr)
+  print(df_dynamic_lr.to_csv())
+  print(df_static_lr.to_csv())
 
   # plot_bits_vs_acc([enet0_dynamic_init_max, enet0_dynamic_init_double_mean,
   #                   enet0_dynamic_init_gaussian], 'figures/dynamic_init.png')