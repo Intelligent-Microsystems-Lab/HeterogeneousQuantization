253ce2567d792ea99e1f403cc533eec395e26029
clee1994 <cschaef6@nd.edu>
2021-11-06 21:26:04 +0000

    normal - lsq sweep

--- examples/efficientnet/configs/run_efficientnet-lite_lsq.sh
+++ examples/efficientnet/configs/run_efficientnet-lite_lsq.sh
@@ -1,4 +1,8 @@
 
+rm -rf ../../../efficientnet-lite0_lsq_int8
+mkdir ../../../efficientnet-lite0_lsq_int8
+python3 train.py --workdir=../../../efficientnet-lite0_lsq_int8 --config=configs/efficientnet-lite0_w4a4_lsq.py  --config.quant.bits=8 --config.learning_rate=0.001
+
 rm -rf ../../../efficientnet-lite0_lsq_int2
 mkdir ../../../efficientnet-lite0_lsq_int2
 python3 train.py --workdir=../../../efficientnet-lite0_lsq_int2 --config=configs/efficientnet-lite0_w4a4_lsq.py  --config.quant.bits=2 --config.batch_norm_epsilon=1e-2
@@ -23,6 +27,3 @@ rm -rf ../../../efficientnet-lite0_lsq_int7
 mkdir ../../../efficientnet-lite0_lsq_int7
 python3 train.py --workdir=../../../efficientnet-lite0_lsq_int7 --config=configs/efficientnet-lite0_w4a4_lsq.py  --config.quant.bits=7 --config.learning_rate=0.001
 
-rm -rf ../../../efficientnet-lite0_lsq_int8
-mkdir ../../../efficientnet-lite0_lsq_int8
-python3 train.py --workdir=../../../efficientnet-lite0_lsq_int8 --config=configs/efficientnet-lite0_w4a4_lsq.py  --config.quant.bits=8 --config.learning_rate=0.001
