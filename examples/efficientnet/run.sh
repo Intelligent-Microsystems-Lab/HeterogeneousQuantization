c2e92640beeee30e860919435f46b6d2e2f7e910
“clee1994” <cschaef6@nd.edu>
2021-12-08 23:04:01 +0000

    normal - irrelevant file removal

--- examples/efficientnet/run.sh
+++
@@ -1,2 +0,0 @@
-python3 train.py --workdir=../../../enet0_int2_init_double_mean_cos_psg  --config=configs/efficientnet-lite0_w2a2_init_double_mean_psg.py
-python3 train.py --workdir=../../../enet0_int2_init_double_mean_cos_ewgs  --config=configs/efficientnet-lite0_w2a2_init_double_mean_ewgs.py
