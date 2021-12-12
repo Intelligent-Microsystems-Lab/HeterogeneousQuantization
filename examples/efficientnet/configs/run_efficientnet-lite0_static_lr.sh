07bd7ed8088b2e34f894b0b091f0351b10889470
Clemens Schaefer <cschaef6@nd.edu>
2021-12-12 17:58:03 -0500

    Update run_efficientnet-lite0_static_lr.sh

--- examples/efficientnet/configs/run_efficientnet-lite0_static_lr.sh
+++ examples/efficientnet/configs/run_efficientnet-lite0_static_lr.sh
@@ -5,6 +5,6 @@ do
   do
     rm -rf ../../efficientnet-lite0_int${BITS}_lr${LR}
     mkdir ../../efficientnet-lite0_int${BITS}_lr${LR}
-    python3 train.py --workdir=../../efficientnet-lite0_int${BITS}_lr${LR} --config=configs/efficientnet/efficientnet-lite0_w2a2.py  --config.quant.bits=${BITS} --config.learning_rate=${LR}
+    python3 train.py --workdir=../../efficientnet-lite0_int${BITS}_lr${LR} --config=efficientnet/configs/efficientnet-lite0_w2a2.py  --config.quant.bits=${BITS} --config.learning_rate=${LR}
   done
 done
