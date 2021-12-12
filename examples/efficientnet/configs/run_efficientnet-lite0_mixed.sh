7d08e0d6c3d022385aaf3df0bdc3a09e6e0e1ec7
Clemens Schaefer <cschaef6@nd.edu>
2021-12-12 01:41:18 -0500

    Update run_efficientnet-lite0_mixed.sh

--- examples/efficientnet/configs/run_efficientnet-lite0_mixed.sh
+++ examples/efficientnet/configs/run_efficientnet-lite0_mixed.sh
@@ -9,8 +9,8 @@ do
   ACT_TARGET=$(echo "$MAX_ACT*$SIZE" | bc -l)
   WEIGHT_TARGET=$(echo "$SUM_WEIGHT*$SIZE" | bc -l)
 
-  rm -rf ../../../efficientnet-lite0_mixed_${SIZE}
-  mkdir ../../../efficientnet-lite0_mixed_${SIZE}
-  python3 train.py --workdir=../../../efficientnet-lite0_mixed_${SIZE} --config=configs/efficientnet-lite0_mixed.py  --config.quant_target.weight_mb=${WEIGHT_TARGET} --config.quant_target.act_mb=${ACT_TARGET}
+  rm -rf ../../efficientnet-lite0_mixed_${SIZE}
+  mkdir ../../efficientnet-lite0_mixed_${SIZE}
+  python3 train.py --workdir=../../efficientnet-lite0_mixed_${SIZE} --config=efficientnet/configs/efficientnet-lite0_mixed.py  --config.quant_target.weight_mb=${WEIGHT_TARGET} --config.quant_target.act_mb=${ACT_TARGET}
 
 done
