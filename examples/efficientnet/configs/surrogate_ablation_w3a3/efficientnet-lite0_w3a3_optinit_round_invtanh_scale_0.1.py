a7333ef4903eb6b3a52c8898702e40f597d64933
Clemens JS Schaefer <cschaef6@nd.edu>
2021-12-14 04:17:45 +0000

    normal - better configs

--- examples/efficientnet/configs/surrogate_ablation_w3a3/efficientnet-lite0_w3a3_optinit_round_invtanh_scale_0.1.py
+++ examples/efficientnet/configs/surrogate_ablation_w3a3/efficientnet-lite0_w3a3_optinit_round_invtanh_scale_0.1.py
@@ -56,6 +56,7 @@ def get_config():
   config.steps_per_eval = -1
 
   config.quant_target = ml_collections.ConfigDict()
+  config.quant_target.size_div = 8. * 1024.
 
   config.quant = ml_collections.ConfigDict()
 
@@ -65,36 +66,36 @@ def get_config():
 
   # Conv for stem layer.
   config.quant.stem = ml_collections.ConfigDict()
-  config.quant.stem.weight = partial(
-      uniform_static, init_fn=partial(gaussian_init), round_fn=round_invtanh)
-  config.quant.stem.act = partial(uniform_static, init_fn=partial(
-      percentile_init, perc=99.9), round_fn=round_invtanh)
+  config.quant.stem.weight = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.stem.act = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(percentile_init, perc=99.9))
 
   # Conv in MBConv blocks.
   config.quant.mbconv = ml_collections.ConfigDict()
-  config.quant.mbconv.weight = partial(
-      uniform_static, init_fn=partial(gaussian_init), round_fn=round_invtanh)
-  config.quant.mbconv.act = partial(uniform_static, init_fn=partial(
-      percentile_init, perc=99.9), round_fn=round_invtanh)
+  config.quant.mbconv.weight = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.mbconv.act = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(percentile_init, perc=99.9))
 
   # Conv for head layer.
   config.quant.head = ml_collections.ConfigDict()
-  config.quant.head.weight = partial(
-      uniform_static, init_fn=partial(gaussian_init), round_fn=round_invtanh)
-  config.quant.head.act = partial(uniform_static, init_fn=partial(
-      percentile_init, perc=99.9), round_fn=round_invtanh)
+  config.quant.head.weight = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.head.act = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(percentile_init, perc=99.9))
 
   # Average quant.
-  config.quant.average = partial(uniform_static, init_fn=partial(
-      percentile_init, perc=99.9), round_fn=round_invtanh)
+  config.quant.average = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(percentile_init, perc=99.9))
 
   # Final linear layer.
   config.quant.dense = ml_collections.ConfigDict()
-  config.quant.dense.weight = partial(
-      uniform_static, init_fn=partial(gaussian_init), round_fn=round_invtanh)
-  config.quant.dense.act = partial(uniform_static, init_fn=partial(
-      percentile_init, perc=99.9), round_fn=round_invtanh)
-  config.quant.dense.bias = partial(
-      uniform_static, init_fn=partial(gaussian_init), round_fn=round_invtanh)
+  config.quant.dense.weight = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.dense.act = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(percentile_init, perc=99.9))
+  config.quant.dense.bias = partial, round_fn = round_invtanh)
+      uniform_static, init_fn=partial(gaussian_init))
 
   return config
--- examples/efficientnet/visualization_enet_bits.py
+++ examples/efficientnet/visualization_enet_bits.py
@@ -26,6 +26,7 @@ import numpy as np
 
 import ml_collections
 from ml_collections import config_flags
+from flax.core import freeze, unfreeze
 
 import sys
 import os.path
@@ -54,6 +55,125 @@ config_flags.DEFINE_config_file(
     lock_config=True)
 
 
+enet_template = {'/stem_conv/weight':-1,
+'/MBConvBlock_0/QuantConv_0/weight':-1,
+'/MBConvBlock_0/QuantConv_0/act':-1,
+'/MBConvBlock_0/depthwise_conv2d/weight':-1,
+'/MBConvBlock_0/depthwise_conv2d/act':-1,
+'/MBConvBlock_1/QuantConv_0/weight':-1,
+'/MBConvBlock_1/QuantConv_0/act':-1,
+'/MBConvBlock_1/QuantConv_1/weight':-1,
+'/MBConvBlock_1/QuantConv_1/act':-1,
+'/MBConvBlock_1/depthwise_conv2d/weight':-1,
+'/MBConvBlock_1/depthwise_conv2d/act':-1,
+'/MBConvBlock_10/QuantConv_0/weight':-1,
+'/MBConvBlock_10/QuantConv_0/act':-1,
+'/MBConvBlock_10/QuantConv_1/weight':-1,
+'/MBConvBlock_10/QuantConv_1/act':-1,
+'/MBConvBlock_10/depthwise_conv2d/weight':-1,
+'/MBConvBlock_10/depthwise_conv2d/act':-1,
+'/MBConvBlock_11/QuantConv_0/weight':-1,
+'/MBConvBlock_11/QuantConv_0/act':-1,
+'/MBConvBlock_11/QuantConv_1/weight':-1,
+'/MBConvBlock_11/QuantConv_1/act':-1,
+'/MBConvBlock_11/depthwise_conv2d/weight':-1,
+'/MBConvBlock_11/depthwise_conv2d/act':-1,
+'/MBConvBlock_12/QuantConv_0/weight':-1,
+'/MBConvBlock_12/QuantConv_0/act':-1,
+'/MBConvBlock_12/QuantConv_1/weight':-1,
+'/MBConvBlock_12/QuantConv_1/act':-1,
+'/MBConvBlock_12/depthwise_conv2d/weight':-1,
+'/MBConvBlock_12/depthwise_conv2d/act':-1,
+'/MBConvBlock_13/QuantConv_0/weight':-1,
+'/MBConvBlock_13/QuantConv_0/act':-1,
+'/MBConvBlock_13/QuantConv_1/weight':-1,
+'/MBConvBlock_13/QuantConv_1/act':-1,
+'/MBConvBlock_13/depthwise_conv2d/weight':-1,
+'/MBConvBlock_13/depthwise_conv2d/act':-1,
+'/MBConvBlock_14/QuantConv_0/weight':-1,
+'/MBConvBlock_14/QuantConv_0/act':-1,
+'/MBConvBlock_14/QuantConv_1/weight':-1,
+'/MBConvBlock_14/QuantConv_1/act':-1,
+'/MBConvBlock_14/depthwise_conv2d/weight':-1,
+'/MBConvBlock_14/depthwise_conv2d/act':-1,
+'/MBConvBlock_15/QuantConv_0/weight':-1,
+'/MBConvBlock_15/QuantConv_0/act':-1,
+'/MBConvBlock_15/QuantConv_1/weight':-1,
+'/MBConvBlock_15/QuantConv_1/act':-1,
+'/MBConvBlock_15/depthwise_conv2d/weight':-1,
+'/MBConvBlock_15/depthwise_conv2d/act':-1,
+'/MBConvBlock_2/QuantConv_0/weight':-1,
+'/MBConvBlock_2/QuantConv_0/act':-1,
+'/MBConvBlock_2/QuantConv_1/weight':-1,
+'/MBConvBlock_2/QuantConv_1/act':-1,
+'/MBConvBlock_2/depthwise_conv2d/weight':-1,
+'/MBConvBlock_2/depthwise_conv2d/act':-1,
+'/MBConvBlock_3/QuantConv_0/weight':-1,
+'/MBConvBlock_3/QuantConv_0/act':-1,
+'/MBConvBlock_3/QuantConv_1/weight':-1,
+'/MBConvBlock_3/QuantConv_1/act':-1,
+'/MBConvBlock_3/depthwise_conv2d/weight':-1,
+'/MBConvBlock_3/depthwise_conv2d/act':-1,
+'/MBConvBlock_4/QuantConv_0/weight':-1,
+'/MBConvBlock_4/QuantConv_0/act':-1,
+'/MBConvBlock_4/QuantConv_1/weight':-1,
+'/MBConvBlock_4/QuantConv_1/act':-1,
+'/MBConvBlock_4/depthwise_conv2d/weight':-1,
+'/MBConvBlock_4/depthwise_conv2d/act':-1,
+'/MBConvBlock_5/QuantConv_0/weight':-1,
+'/MBConvBlock_5/QuantConv_0/act':-1,
+'/MBConvBlock_5/QuantConv_1/weight':-1,
+'/MBConvBlock_5/QuantConv_1/act':-1,
+'/MBConvBlock_5/depthwise_conv2d/weight':-1,
+'/MBConvBlock_5/depthwise_conv2d/act':-1,
+'/MBConvBlock_6/QuantConv_0/weight':-1,
+'/MBConvBlock_6/QuantConv_0/act':-1,
+'/MBConvBlock_6/QuantConv_1/weight':-1,
+'/MBConvBlock_6/QuantConv_1/act':-1,
+'/MBConvBlock_6/depthwise_conv2d/weight':-1,
+'/MBConvBlock_6/depthwise_conv2d/act':-1,
+'/MBConvBlock_7/QuantConv_0/weight':-1,
+'/MBConvBlock_7/QuantConv_0/act':-1,
+'/MBConvBlock_7/QuantConv_1/weight':-1,
+'/MBConvBlock_7/QuantConv_1/act':-1,
+'/MBConvBlock_7/depthwise_conv2d/weight':-1,
+'/MBConvBlock_7/depthwise_conv2d/act':-1,
+'/MBConvBlock_8/QuantConv_0/weight':-1,
+'/MBConvBlock_8/QuantConv_0/act':-1,
+'/MBConvBlock_8/QuantConv_1/weight':-1,
+'/MBConvBlock_8/QuantConv_1/act':-1,
+'/MBConvBlock_8/depthwise_conv2d/weight':-1,
+'/MBConvBlock_8/depthwise_conv2d/act':-1,
+'/MBConvBlock_9/QuantConv_0/weight':-1,
+'/MBConvBlock_9/QuantConv_0/act':-1,
+'/MBConvBlock_9/QuantConv_1/weight':-1,
+'/MBConvBlock_9/QuantConv_1/act':-1,
+'/MBConvBlock_9/depthwise_conv2d/weight':-1,
+'/MBConvBlock_9/depthwise_conv2d/act':-1,
+'/head_conv/weight':-1,
+'/head_conv/act':-1,
+'/act':-1,
+'/QuantDense_0/weight':-1,
+'/QuantDense_0/act':-1,
+'/QuantDense_0/parametric_d_xmax_2':-1,
+}
+
+
+def flatten_names(x, path):
+  if hasattr(x, 'keys'):
+    list_x = []
+    for y in x.keys():
+      list_x += flatten_names(x[y], path+'/'+y)
+    return list_x
+  return [path]
+
+def fetch_value(x, name_list):
+  if len(name_list) == 1:
+    return x[name_list[0]]
+  else:
+    return fetch_value(x[name_list[0]], name_list[1:])
+
+
 def plot_bits(config: ml_collections.ConfigDict,workdir: str):
   rng = random.PRNGKey(config.seed)
 
@@ -65,46 +185,77 @@ def plot_bits(config: ml_collections.ConfigDict,workdir: str):
       subkey, config, model, config.image_size, lambda x: x)
   state = restore_checkpoint(state, workdir)
 
+  
+  param_names = flatten_names(unfreeze(state.params['quant_params']), '')
+  flat_params = {}
+  for local_name in  param_names:
+    flat_params[local_name] = fetch_value(state.params['quant_params'], local_name[1:].split('/'))
 
-  xmax = jax.tree_util.tree_flatten(state.params['quant_params'])[0][::2]
-  d = jax.tree_util.tree_flatten(state.params['quant_params'])[0][1::2]
+
+  
+  xmax = jnp.array(jax.tree_util.tree_flatten(state.params['quant_params'])[0][::2])
+  d = jnp.array(jax.tree_util.tree_flatten(state.params['quant_params'])[0][1::2])
   
 
   font_size = 22
 
-  fig, ax = plt.subplots(nrows = 2, ncols =1, figsize=(12, 8.8))
+  fig, ax = plt.subplots(nrows = 3, ncols =1, figsize=(12, 8.8))
   ax[0].spines["top"].set_visible(False)
   ax[0].spines["right"].set_visible(False)
   ax[1].spines["top"].set_visible(False)
   ax[1].spines["right"].set_visible(False)
+  ax[2].spines["top"].set_visible(False)
+  ax[2].spines["right"].set_visible(False)
 
   ax[0].xaxis.set_tick_params(width=5, length=10, labelsize=font_size)
   ax[0].yaxis.set_tick_params(width=5, length=10, labelsize=font_size)
   ax[1].xaxis.set_tick_params(width=5, length=10, labelsize=font_size)
   ax[1].yaxis.set_tick_params(width=5, length=10, labelsize=font_size)
+  ax[2].xaxis.set_tick_params(width=5, length=10, labelsize=font_size)
+  ax[2].yaxis.set_tick_params(width=5, length=10, labelsize=font_size)
 
   for axis in ['top', 'bottom', 'left', 'right']:
     ax[0].spines[axis].set_linewidth(5)
     ax[1].spines[axis].set_linewidth(5)
+    ax[2].spines[axis].set_linewidth(5)
 
-  # for tick in ax.xaxis.get_major_ticks():
-  #   tick.label1.set_fontweight('bold')
-  # for tick in ax.yaxis.get_major_ticks():
-  #   tick.label1.set_fontweight('bold')
+  for tick in ax[0].xaxis.get_major_ticks():
+    tick.label1.set_fontweight('bold')
+  for tick in ax[0].yaxis.get_major_ticks():
+    tick.label1.set_fontweight('bold')
 
-  import pdb; pdb.set_trace()
-  ax[0].bar(np.arange(len(xmax)), xmax)
-  ax[1].bar(np.arange(len(d)), d)
+  for tick in ax[1].xaxis.get_major_ticks():
+    tick.label1.set_fontweight('bold')
+  for tick in ax[1].yaxis.get_major_ticks():
+    tick.label1.set_fontweight('bold')
 
-  ax[0].set_xlabel("Layer", fontsize=font_size, fontweight='bold')
-  ax[0].set_ylabel("Bit Width", fontsize=font_size, fontweight='bold')
-  # plt.legend(
-  #     bbox_to_anchor=(0.5, 1.2),
-  #     loc="upper center",
-  #     ncol=2,
-  #     frameon=False,
-  #     prop={'weight': 'bold', 'size': font_size}
-  # )
+  for tick in ax[2].xaxis.get_major_ticks():
+    tick.label1.set_fontweight('bold')
+  for tick in ax[2].yaxis.get_major_ticks():
+    tick.label1.set_fontweight('bold')
+
+  ax[0].bar(np.arange(len(xmax[:10])), xmax[:10], color = 'red', label='Activations')
+  ax[0].bar(np.arange(len(xmax[10:])), xmax[10:], color = 'green', label = 'Weights')
+
+  ax[1].bar(np.arange(len(d[:10])), d[:10], color = 'red', label = 'Activations')
+  ax[1].bar(np.arange(len(d[10:])), d[10:], color = 'green', label = 'Weights')
+
+  ax[2].bar(np.arange(len(d[:10])), np.log2(xmax[:10]/d[:10]), color = 'red', label = 'Activations')
+  ax[2].bar(np.arange(len(d[10:])), np.log2(xmax[10:]/d[10:]), color = 'green', label = 'Weights')
+
+  ax[1].set_xlabel("layer", fontsize=font_size, fontweight='bold')
+  ax[1].set_ylabel("bits", fontsize=font_size, fontweight='bold')
+  ax[1].set_xlabel("layer", fontsize=font_size, fontweight='bold')
+  ax[1].set_ylabel("d", fontsize=font_size, fontweight='bold')
+  ax[0].set_xlabel("layer", fontsize=font_size, fontweight='bold')
+  ax[0].set_ylabel("xmax", fontsize=font_size, fontweight='bold')
+  plt.legend(
+      # bbox_to_anchor=(0.5, 1.2),
+      # loc="upper center",
+      ncol=2,
+      frameon=False,
+      prop={'weight': 'bold', 'size': font_size}
+  )
   plt.tight_layout()
   plt.savefig('efficientnet/figures/bitwidths.png')
   plt.close()
