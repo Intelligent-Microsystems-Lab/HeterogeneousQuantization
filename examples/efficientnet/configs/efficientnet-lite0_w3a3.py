31fe1e2b8499e5599a45b361a5223a98568c6be7
“clee1994” <cschaef6@nd.edu>
2021-12-15 16:52:45 +0000

    normal - 3 bit config

--- examples/efficientnet/configs/efficientnet-lite0_w3a3.py
+++ examples/efficientnet/configs/efficientnet-lite0_w3a3.py
@@ -17,10 +17,9 @@ def get_config():
 
   # As defined in the `models` module.
   config.model = 'EfficientNetB0'
-
   # `name` argument of tensorflow_datasets.builder()
-  config.cache = True
   config.dataset = 'imagenet2012'
+  config.num_classes = 1000
   config.tfds_data_dir = 'gs://imagenet_clemens/tensorflow_datasets'
   config.image_size = 224
   config.crop_padding = 32
@@ -33,26 +32,32 @@ def get_config():
   config.mean_rgb = [127.0, 127.0, 127.0]
   config.stddev_rgb = [128.0, 128.0, 128.0]
 
-  config.num_classes = 1000
-
-  # Load pretrained weights.
-  config.pretrained = "../../../pretrained_efficientnet/efficientnet-lite0"
-
+  config.optimizer = 'rmsprop'
   config.learning_rate = 0.01
-  config.warmup_epochs = 2  # for optimizer to settle in
-  config.weight_decay = 1e-5
+  config.lr_boundaries_scale = None
+  config.warmup_epochs = 2.0
   config.momentum = 0.9
   config.batch_size = 2048
+  config.weight_decay = 0.00001
+  config.nesterov = True
+  config.smoothing = .1
 
   config.num_epochs = 50
   config.log_every_steps = 256
+
+  config.cache = True
+
+  # Load pretrained weights.
+  config.pretrained = "../../pretrained_efficientnet/efficientnet-lite0"
 
   # If num_train_steps==-1 then the number of training steps is calculated from
   # num_epochs using the entire dataset. Similarly for steps_per_eval.
   config.num_train_steps = -1
   config.steps_per_eval = -1
 
+
   config.quant_target = ml_collections.ConfigDict()
+  config.quant_target.size_div = 8. * 1024.
 
   config.quant = ml_collections.ConfigDict()
 
@@ -62,26 +67,34 @@ def get_config():
 
   # Conv for stem layer.
   config.quant.stem = ml_collections.ConfigDict()
-  config.quant.stem.weight = partial(uniform_static)
-  config.quant.stem.act = partial(uniform_static)
+  config.quant.stem.weight = partial(
+      uniform_static, init_fn=partial(gaussian_init))
 
   # Conv in MBConv blocks.
   config.quant.mbconv = ml_collections.ConfigDict()
-  config.quant.mbconv.weight = partial(uniform_static)
-  config.quant.mbconv.act = partial(uniform_static)
+  config.quant.mbconv.weight = partial(
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.mbconv.act = partial(
+      uniform_static, act=True, init_fn=partial(percentile_init, perc=99.99))
 
   # Conv for head layer.
   config.quant.head = ml_collections.ConfigDict()
-  config.quant.head.weight = partial(uniform_static)
-  config.quant.head.act = partial(uniform_static)
+  config.quant.head.weight = partial(
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.head.act = partial(
+      uniform_static, act=True, init_fn=partial(percentile_init, perc=99.99))
 
   # Average quant.
-  config.quant.average = partial(uniform_static)
+  config.quant.average = partial(
+      uniform_static, act=True, init_fn=partial(percentile_init, perc=99.99))
 
   # Final linear layer.
   config.quant.dense = ml_collections.ConfigDict()
-  config.quant.dense.weight = partial(uniform_static)
-  config.quant.dense.act = partial(uniform_static)
-  config.quant.dense.bias = partial(uniform_static)
+  config.quant.dense.weight = partial(
+      uniform_static, init_fn=partial(gaussian_init))
+  config.quant.dense.act = partial(
+      uniform_static, act=True, init_fn=partial(percentile_init, perc=99.99))
+  config.quant.dense.bias = partial(
+      uniform_static, init_fn=partial(gaussian_init))
 
   return config
