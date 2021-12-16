6d0e00df755775bb3a58d88c167b7fcc534ce506
Clemens Schaefer <cschaef6@nd.edu>
2021-12-15 23:40:36 -0500

    Update efficientnet-lite0_mixed.py

--- examples/efficientnet/configs/efficientnet-lite0_mixed.py
+++ examples/efficientnet/configs/efficientnet-lite0_mixed.py
@@ -58,10 +58,10 @@ def get_config():
   config.quant_target = ml_collections.ConfigDict()
 
   config.quant_target.weight_mb = 1250.0
-  config.quant_target.weight_penalty = .0005
+  config.quant_target.weight_penalty = .0001
   config.quant_target.act_mode = 'sum'
   config.quant_target.act_mb = 3600.0
-  config.quant_target.act_penalty = .0005
+  config.quant_target.act_penalty = .0001
   config.quant_target.size_div = 8. * 1024.
   config.quant_target.eval_start = 61000  # 31050
 
