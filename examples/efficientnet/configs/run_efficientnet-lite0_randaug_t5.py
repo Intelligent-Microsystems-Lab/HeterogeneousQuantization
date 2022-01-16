f499702f48a3e6419798abc85928ee50d9375aa8
Clemens Schaefer <cschaef6@nd.edu>
2022-01-15 21:34:44 -0500

    Update run_efficientnet-lite0_randaug_t5.py

--- examples/efficientnet/configs/run_efficientnet-lite0_randaug_t5.py
+++ examples/efficientnet/configs/run_efficientnet-lite0_randaug_t5.py
@@ -4,6 +4,6 @@ do
 
   rm -rf ../../efficientnet-lite0_randaug_${CONFIG}
   mkdir ../../efficientnet-lite0_randaug_${CONFIG}
-  python3 train.py --workdir=../../efficientnet-lite0_randaug_${CONFIG} --config=efficientnet/configs/efficientnet-lite0.py  --config.augment_name=randaugment_{$CONFIG}
+  python3 train.py --workdir=../../efficientnet-lite0_randaug_${CONFIG} --config=efficientnet/configs/efficientnet-lite0.py  --config.augment_name=randaugment_$CONFIG
 
 done
