d9a7fd07b1ec1fe982cd3e28c5b1a866fba05af8
clee1994 <cschaef6@nd.edu>
2022-02-08 22:23:39 +0000

    normal - test

--- examples/efficientnet/visualization.py
+++ examples/efficientnet/visualization.py
@@ -626,8 +626,8 @@ def plot_surrogate_mix():
              linewidths=5, s=840, color='green')
 
 
-  plt.axhline(y=65.741000, color='orange', linestyle='--', label = 'STE')
-  plt.xticks(base_x / 2 + .1, names, rotation='horizontal')
+  plt.axhline(y=65.741000, color='orange', linestyle='--', label = 'STE', linewidths=5)
+  plt.xticks(base_x / 2 + .1, names, rotation=45)
 
 
   plt.legend(
