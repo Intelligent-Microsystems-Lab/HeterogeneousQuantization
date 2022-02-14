95541906bccc8e571c0be2c8ee305477b2f1f314
clee1994 <cschaef6@nd.edu>
2022-02-14 02:09:20 +0000

    normal - run different bits

--- examples/efficientnet/configs/run_efficientnet-lite0_20.sh
+++ examples/efficientnet/configs/run_efficientnet-lite0_20.sh
@@ -1,5 +1,6 @@
 
 # weight_act
+NAME="STE"
 # NAME="ste_psgd"
 # NAME="ste_ewgs"
 # NAME="ste_invtanh"
@@ -14,7 +15,7 @@
 counter=0
 for SEED in 193012823 235899598 8627169 103372330 14339038 7066636907367338630 6341454152401905754 1959330209 4275582192035986655 5329177101860618450 649323830 341384131 980310836 749032139 251745660 891479909 536363141 362693951 8488585 29492181
 do
-  python3 train.py --workdir=../../efficientnet-lite0_w3a3_${NAME}_${counter} --config=efficientnet/configs/efficientnet-lite0_w3a3_${NAME}.py --config.seed=$SEED
-  gsutil cp -r ./../efficientnet-lite0_mixed_${NAME}_${counter} gs://imagenet_clemens/surrogate_sweeps
+  python3 train.py --workdir=../../efficientnet-lite0_w3a3_${NAME}_${counter}_bits2 --config=efficientnet/configs/efficientnet-lite0_w3a3_${NAME}.py --config.seed=$SEED --config.quant.bits=2
+  gsutil cp -r ./../efficientnet-lite0_mixed_${NAME}_${counter}_bits2 gs://imagenet_clemens/surrogate_sweeps
   counter=$((counter+1))
 done
