b37cc0a8eecd6ee8fcd604eb1b39fc5b0b10c269
Clemens Schaefer <cschaef6@nd.edu>
2022-02-22 00:25:48 -0500

    Delete examples/efficientnet/configs/surrogate_ablation_psgd_w3a3 directory

--- examples/efficientnet/configs/surrogate_ablation_psgd_w3a3/run_round_ablation_1.sh
+++
@@ -1,6 +0,0 @@
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_psgd_scale_2.0 --config=efficientnet/configs/surrogate_ablation_psgd_w3a3/efficientnet-lite0_w3a3_round_psgd_scale_2.0.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_psgd_scale_1.0 --config=efficientnet/configs/surrogate_ablation_psgd_w3a3/efficientnet-lite0_w3a3_round_psgd_scale_1.0.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_psgd_scale_0.1 --config=efficientnet/configs/surrogate_ablation_psgd_w3a3/efficientnet-lite0_w3a3_round_psgd_scale_0.1.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_psgd_scale_0.01 --config=efficientnet/configs/surrogate_ablation_psgd_w3a3/efficientnet-lite0_w3a3_round_psgd_scale_0.01.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_psgd_scale_0.001 --config=efficientnet/configs/surrogate_ablation_psgd_w3a3/efficientnet-lite0_w3a3_round_psgd_scale_0.001.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_psgd_scale_0.0001 --config=efficientnet/configs/surrogate_ablation_psgd_w3a3/efficientnet-lite0_w3a3_round_psgd_scale_0.0001.py
