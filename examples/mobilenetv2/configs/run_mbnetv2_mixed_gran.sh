61fbd103b3ffd500e9b2b1b6f5af42b1e05ca919
Clemens Schaefer <cschaef6@nd.edu>
2022-02-22 19:01:48 +0000

    normal - gran mbnet mixed

---
+++ examples/mobilenetv2/configs/run_mbnetv2_mixed_gran.sh
@@ -0,0 +1,21 @@
+
+
+MAX_ACT=993.0
+SUM_WEIGHT=434.0
+
+
+for SIZE in 2 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8 3.9 4
+do
+
+  ACT_TARGET=$(echo "$MAX_ACT*$SIZE" | bc -l)
+  WEIGHT_TARGET=$(echo "$SUM_WEIGHT*$SIZE" | bc -l)
+  BITS=$(echo "scale=0; ($SIZE)/1 + 2" | bc -l)
+
+  python3 train.py --workdir=../../mbnetv2_mixed_${SIZE}_gran_9 --config=mobilenetv2/configs/mobilenetv2_mixed_gran.py  --config.quant_target.weight_mb=${WEIGHT_TARGET} --config.quant_target.act_mb=${ACT_TARGET} --config.quant.bits=${BITS} --config.pretrained_quant=gs://imagenet_clemens/pretrained/mbnet_${BITS}_pre_gran_2/best
+  if [ -d ../../mbnetv2_mixed_${SIZE}_gran_9/best ]; then
+    python3 train.py --workdir=../../mbnetv2_mixed_${SIZE}_finetune_gran_9 --config=mobilenetv2/configs/mobilenetv2_mixed_gran_finetune.py --config.pretrained_quant=../../mbnetv2_mixed_${SIZE}_gran_9/best
+  else
+    python3 train.py --workdir=../../mbnetv2_mixed_${SIZE}_finetune_gran_9 --config=mobilenetv2/configs/mobilenetv2_mixed_gran_finetune.py --config.pretrained_quant=../../mbnetv2_mixed_${SIZE}_gran_9/
+  fi
+
+done
