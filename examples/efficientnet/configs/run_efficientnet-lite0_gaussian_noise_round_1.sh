64450fbd6dd6024de66215d9fa2940a28aad0488
Clemens Schaefer <cschaef6@nd.edu>
2022-02-22 00:27:17 -0500

    Delete run_efficientnet-lite0_gaussian_noise_round_1.sh

--- examples/efficientnet/configs/run_efficientnet-lite0_gaussian_noise_round_1.sh
+++
@@ -1,8 +0,0 @@
-
-for SCALE in 0.01 0.001 0.0001 0.00001
-do
-
-  rm -rf ../../efficientnet-lite0_noise_bwd_${SCALE}_${SEED}
-  mkdir ../../efficientnet-lite0_noise_bwd_${SCALE}_${SEED}
-  python3 train.py --workdir=../../efficientnet-lite0_noise_bwd_${SCALE}_${SEED} --config=efficientnet/configs/efficientnet-lite0_w3a3_round_bwd_noise.py  --config.seed=8627169 --config.quant.g_scale=${SCALE}
-done
