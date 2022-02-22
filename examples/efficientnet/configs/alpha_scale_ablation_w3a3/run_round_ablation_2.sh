37c188db4f52692714fb917ec8b9b543791b0b3c
Clemens Schaefer <cschaef6@nd.edu>
2022-02-22 00:25:34 -0500

    Delete examples/efficientnet/configs/alpha_scale_ablation_w3a3 directory

--- examples/efficientnet/configs/alpha_scale_ablation_w3a3/run_round_ablation_2.sh
+++
@@ -1,6 +0,0 @@
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.4 --config=efficientnet/configs/alpha_scale_ablation_w3a3/efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.4.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.3 --config=efficientnet/configs/alpha_scale_ablation_w3a3/efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.3.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.2 --config=efficientnet/configs/alpha_scale_ablation_w3a3/efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.2.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_tanh_alpha_scale_1.0 --config=efficientnet/configs/alpha_scale_ablation_w3a3/efficientnet-lite0_w3a3_round_tanh_alpha_scale_1.0.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.0 --config=efficientnet/configs/alpha_scale_ablation_w3a3/efficientnet-lite0_w3a3_round_tanh_alpha_scale_0.0.py
-python3 train.py --workdir=../../efficientnet-lite0_w3a3_round_invtanh_alpha_scale_1.0 --config=efficientnet/configs/alpha_scale_ablation_w3a3/efficientnet-lite0_w3a3_round_invtanh_alpha_scale_1.0.py
