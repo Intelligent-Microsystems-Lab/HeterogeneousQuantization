481fee05112db7b2d78695eb10157dc1c21dccc0
clee1994 <cschaef6@nd.edu>
2022-03-29 16:32:32 +0000

    normal - new config

--- examples/squeezenext/configs/run_sqnxt23_w2_mixed_gran.sh
+++ examples/squeezenext/configs/run_sqnxt23_w2_mixed_gran.sh
@@ -10,11 +10,11 @@ do
   WEIGHT_TARGET=$(echo "$SUM_WEIGHT*$SIZE" | bc -l)
   BITS=$(echo "scale=0; ($SIZE)/1 + 2" | bc -l)
 
-  python3 train.py --workdir=../../sqnxt23_w2_mixed_${SIZE}_7 --config=squeezenext/configs/sqnxt23_w2_mixed_gran.py  --config.quant_target.weight_mb=${WEIGHT_TARGET} --config.quant_target.act_mb=${ACT_TARGET} --config.quant.bits=${BITS} --config.pretrained_quant=gs://imagenet_clemens/pretrained_hq/sqnxt_${BITS}_gran_pre_3/best
-  if [ -d ../../sqnxt23_w2_mixed_${SIZE}_7/best ]; then
-    python3 train.py --workdir=../../sqnxt23_w2_mixed_${SIZE}_finetune_7 --config=squeezenext/configs/sqnxt23_w2_mixed_finetune_gran.py --config.pretrained_quant=../../sqnxt23_w2_mixed_${SIZE}_7/best
+  python3 train.py --workdir=../../sqnxt23_w2_mixed_${SIZE}_gran_7 --config=squeezenext/configs/sqnxt23_w2_mixed_gran.py  --config.quant_target.weight_mb=${WEIGHT_TARGET} --config.quant_target.act_mb=${ACT_TARGET} --config.quant.bits=${BITS} --config.pretrained_quant=gs://imagenet_clemens/pretrained_hq/sqnxt_${BITS}_gran_pre_3/best
+  if [ -d ../../sqnxt23_w2_mixed_${SIZE}_gran_7/best ]; then
+    python3 train.py --workdir=../../sqnxt23_w2_mixed_${SIZE}_finetune_gran_7 --config=squeezenext/configs/sqnxt23_w2_mixed_finetune_gran.py --config.pretrained_quant=../../sqnxt23_w2_mixed_${SIZE}_gran_7/best
   else
-    python3 train.py --workdir=../../sqnxt23_w2_mixed_${SIZE}_finetune_7 --config=squeezenext/configs/sqnxt23_w2_mixed_finetune_gran.py --config.pretrained_quant=../../sqnxt23_w2_mixed_${SIZE}_7/
+    python3 train.py --workdir=../../sqnxt23_w2_mixed_${SIZE}_finetune_gran_7 --config=squeezenext/configs/sqnxt23_w2_mixed_finetune_gran.py --config.pretrained_quant=../../sqnxt23_w2_mixed_${SIZE}_gran_7/
   fi
 
 done
