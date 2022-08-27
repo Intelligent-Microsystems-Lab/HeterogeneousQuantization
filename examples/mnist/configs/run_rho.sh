1206260cbcd75642b8bd8ef692f1977529c8ca2b
Clemens Schaefer <15638937+clee1994@users.noreply.github.com>
2022-08-27 12:24:15 -0400

    normal - test checkin

--- examples/mnist/configs/run_rho.sh
+++ examples/mnist/configs/run_rho.sh
@@ -1,5 +1,5 @@
 for RHO in 0. 0.01 0.1 0.5 1.0 1.5 2.0 5.0 10.
 do
   rm -rf ../../mnist_admm_${RHO}
-  python3 train.py --workdir=../../mnist_admm_${RHO} --config=mnist/configs/mnist_admm.py --config.rho=${RHO}
+  python3 train.py --workdir=../../mnist_admm_t1_${RHO} --config=mnist/configs/mnist_admm.py --config.rho=${RHO}
 done
