bf6b593c30f07c124df4c157f4c72f6d963794a0
clee1994 <cschaef6@nd.edu>
2022-08-30 23:55:55 +0000

    normal - test

---
+++ examples/resnet/configs/resnet101_fp32.py
@@ -0,0 +1,79 @@
+# IMSL Lab - University of Notre Dame
+# Author: Clemens JS Schaefer
+# Originally copied from https://github.com/google/flax/tree/main/examples
+
+"""Default Hyperparameter configuration."""
+
+import ml_collections
+from functools import partial
+
+
+def get_config():
+  """Get the default hyperparameter configuration."""
+  config = ml_collections.ConfigDict()
+
+  config.seed = 1349194
+
+  # As defined in the `models` module.
+  config.model = 'ResNet101'
+  # `name` argument of tensorflow_datasets.builder()
+  config.dataset = 'imagenet2012'
+  config.num_classes = 1000
+  config.tfds_data_dir = 'gs://imagenet_clemens/tensorflow_datasets'
+  config.image_size = 224
+  config.crop_padding = 32
+
+  # Mean and std style for pre-processing.
+  # config.mean_rgb = [0.485 * 255, 0.456 * 255, 0.406 * 255]
+  # config.stddev_rgb = [0.229 * 255, 0.224 * 255, 0.225 * 255]
+
+  # Edge models use inception-style MEAN & STDDEV for better post-quantization.
+  config.mean_rgb = [127.0, 127.0, 127.0]
+  config.stddev_rgb = [128.0, 128.0, 128.0]
+  config.augment_name = 'plain'
+
+  config.optimizer = 'sgd'
+  config.learning_rate = 0.25
+  config.lr_boundaries_scale = None
+  config.warmup_epochs = 5.0
+  config.momentum = 0.9
+  config.batch_size = 512
+  config.weight_decay = 0.0001
+  config.nesterov = True
+  config.smoothing = .1
+
+  config.num_epochs = 100.0
+  config.log_every_steps = 100
+
+  config.cache = False
+  config.half_precision = False
+
+  config.pretrained = None  # '../../pretrained_resnet/resnet18_v2'
+  config.pretrained_quant = None
+
+  # If num_train_steps==-1 then the number of training steps is calculated from
+  # num_epochs using the entire dataset. Similarly for steps_per_eval.
+  config.num_train_steps = -1
+  config.steps_per_eval = -1
+
+  config.quant_target = ml_collections.ConfigDict()
+  config.quant_target.size_div = 8. * 1000.
+  config.quant_target.update_every = 1
+
+  config.quant = ml_collections.ConfigDict()
+
+  config.quant.a_bits = 32
+  config.quant.w_bits = 32
+
+  config.quant.g_scale = 0.
+
+  # Conv for stem layer.
+  config.quant.stem = ml_collections.ConfigDict()
+
+  # Conv in MBConv blocks.
+  config.quant.mbconv = ml_collections.ConfigDict()
+
+  # Final linear layer.
+  config.quant.dense = ml_collections.ConfigDict()
+
+  return config
