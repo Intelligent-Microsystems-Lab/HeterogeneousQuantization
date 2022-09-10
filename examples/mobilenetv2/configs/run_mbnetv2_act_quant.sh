a2003aa0f1e4175ffe096e963e7eccbea2a5b503
“clee1994” <cschaef6@nd.edu>
2022-09-10 03:04:43 +0000

    normal - test

--- examples/mobilenetv2/configs/run_mbnetv2_act_quant.sh
+++ examples/mobilenetv2/configs/run_mbnetv2_act_quant.sh
@@ -1,5 +1,5 @@
 
-for BITS in 8 7 6 5 4 3 2
+for BITS in 5 4
 do
   python3 distillation_finetune.py --workdir=../../mbnet2_act_${BITS} --config=mobilenetv2/configs/mobilenetv2_a_int${BITS}_distillation.py
 done
